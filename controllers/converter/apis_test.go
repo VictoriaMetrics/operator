13c6fec337f66ac96d09e14c01150861d18845f1
f41gh7 <nik@victoriametrics.com>
2020-10-05 03:37:55 +0300

    added tests for prometheus converter

--- controllers/converter/apis_test.go
+++ controllers/converter/apis_test.go
@@ -1,9 +1,11 @@
 package converter
 
 import (
+	"reflect"
+	"testing"
+
 	v1beta1vm "github.com/VictoriaMetrics/operator/api/v1beta1"
 	v1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
-	"testing"
 )
 
 func TestConvertTlsConfig(t *testing.T) {
@@ -36,6 +38,149 @@ func TestConvertTlsConfig(t *testing.T) {
 			got := ConvertTlsConfig(tt.args.tlsConf)
 			if got.KeyFile != tt.want.KeyFile || got.CertFile != tt.want.CertFile || got.CAFile != tt.want.CAFile {
 				t.Errorf("ConvertTlsConfig() = \n%v, \nwant \n%v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConvertRelabelConfig(t *testing.T) {
+	type args struct {
+		promRelabelConfig []*v1.RelabelConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want []*v1beta1vm.RelabelConfig
+	}{
+		{
+			name: "test empty cfg",
+			args: args{},
+			want: nil,
+		},
+		{
+			name: "1 relabel cfg rule",
+			args: args{
+				promRelabelConfig: []*v1.RelabelConfig{
+					{
+						Action:       "drop",
+						SourceLabels: []string{"__address__"},
+					},
+				},
+			},
+			want: []*v1beta1vm.RelabelConfig{
+				{
+					Action:       "drop",
+					SourceLabels: []string{"__address__"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConvertRelabelConfig(tt.args.promRelabelConfig)
+			if len(got) != len(tt.want) {
+				t.Fatalf("len of relabelConfigs mismatch, want: %d, got %d", len(tt.want), len(got))
+			}
+			for i, wantRelabelConfig := range tt.want {
+				if !reflect.DeepEqual(*wantRelabelConfig, *got[i]) {
+					t.Fatalf("ConvertRelabelConfig() = %v, want %v", *got[i], *wantRelabelConfig)
+				}
+			}
+		})
+	}
+}
+
+func TestConvertEndpoint(t *testing.T) {
+	type args struct {
+		promEndpoint []v1.Endpoint
+	}
+	tests := []struct {
+		name string
+		args args
+		want []v1beta1vm.Endpoint
+	}{
+		{
+			name: "convert endpoint with relabel config",
+			args: args{
+				promEndpoint: []v1.Endpoint{
+					{
+						Port: "9100",
+						Path: "/metrics",
+						RelabelConfigs: []*v1.RelabelConfig{
+							{
+								Action: "drop",
+							},
+						},
+					},
+				},
+			},
+			want: []v1beta1vm.Endpoint{
+				{
+					Path: "/metrics",
+					Port: "9100",
+					RelabelConfigs: []*v1beta1vm.RelabelConfig{
+						{
+							Action: "drop",
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ConvertEndpoint(tt.args.promEndpoint); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ConvertEndpoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConvertServiceMonitor(t *testing.T) {
+	type args struct {
+		serviceMon *v1.ServiceMonitor
+	}
+	tests := []struct {
+		name string
+		args args
+		want v1beta1vm.VMServiceScrape
+	}{
+		{
+			name: "with metricsRelabelConfig",
+			args: args{
+				serviceMon: &v1.ServiceMonitor{
+					Spec: v1.ServiceMonitorSpec{
+						Endpoints: []v1.Endpoint{
+							{
+								MetricRelabelConfigs: []*v1.RelabelConfig{
+									{
+										Action: "drop",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: v1beta1vm.VMServiceScrape{
+				Spec: v1beta1vm.VMServiceScrapeSpec{
+					Endpoints: []v1beta1vm.Endpoint{
+						{
+							MetricRelabelConfigs: []*v1beta1vm.RelabelConfig{
+								{
+									Action: "drop",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ConvertServiceMonitor(tt.args.serviceMon); !reflect.DeepEqual(*got, tt.want) {
+				t.Errorf("ConvertServiceMonitor() = %v, want %v", got, tt.want)
 			}
 		})
 	}

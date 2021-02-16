10798378c62268a5dee3964a16bcf5557fd3b0c8
f41gh7 <nik@victoriametrics.com>
2021-02-16 17:07:26 +0300

    fixes vmalert config

--- controllers/vmalert_controller.go
+++ controllers/vmalert_controller.go
@@ -105,6 +105,7 @@ func (r *VMAlertReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&appsv1.Deployment{}).
 		Owns(&victoriametricsv1beta1.VMServiceScrape{}).
 		Owns(&v1.ConfigMap{}).
+		Owns(&v1.Secret{}).
 		Owns(&v1.Service{}).
 		Owns(&v1.ServiceAccount{}).
 		Complete(r)

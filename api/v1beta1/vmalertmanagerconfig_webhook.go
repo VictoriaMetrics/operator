VictoriaMetrics/operator

commit 5030eaf4238e405a51de71c9d64eead9a47a8d8f

api: changes errors handling for objects unmarshal due to the user input errors and unmarshall errors, which prevents operator to work normally new ParsingError field was introduced for object Specs it must be checked before working with object and contains error with needed unmarshalling context

api/v1beta1/vmalertmanagerconfig_webhook.go
@@ -46,23 +46,33 @@ var _ webhook.Validator = &VMAlertmanagerConfig{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateCreate() error {
-	vmalertmanagerconfiglog.Info("validate create", "name", r.Name)
-
+	if r.Spec.ParsingError != "" {
+		return fmt.Errorf(r.Spec.ParsingError)
+	}
+	if mustSkipValidation(r) {
+		return nil
+	}
+	if err := r.Validate(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateUpdate(old runtime.Object) error {
-	vmalertmanagerconfiglog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
+	if r.Spec.ParsingError != "" {
+		return fmt.Errorf(r.Spec.ParsingError)
+	}
+	if mustSkipValidation(r) {
+		return nil
+	}
+	if err := r.Validate(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateDelete() error {
-	vmalertmanagerconfiglog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }

api/victoriametrics/v1beta1/vmalertmanagerconfig_webhook.go
@@ -46,23 +46,33 @@ var _ webhook.Validator = &VMAlertmanagerConfig{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateCreate() error {
-	vmalertmanagerconfiglog.Info("validate create", "name", r.Name)
-
+	if r.Spec.ParsingError != "" {
+		return fmt.Errorf(r.Spec.ParsingError)
+	}
+	if mustSkipValidation(r) {
+		return nil
+	}
+	if err := r.Validate(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateUpdate(old runtime.Object) error {
-	vmalertmanagerconfiglog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
+	if r.Spec.ParsingError != "" {
+		return fmt.Errorf(r.Spec.ParsingError)
+	}
+	if mustSkipValidation(r) {
+		return nil
+	}
+	if err := r.Validate(); err != nil {
+		return err
+	}
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *VMAlertmanagerConfig) ValidateDelete() error {
-	vmalertmanagerconfiglog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }

commit 4a7e87368209e98de21cfb64d88eabf2da87f71f

api/vmalertmanagerconfig: fixes regression at config generation issue related to regression after 5030eaf4238e405a51de71c9d64eead9a47a8d8f Validate() call mutates data and it leads to the configuration duplicates https://github.com/VictoriaMetrics/operator/issues/554

api/v1beta1/vmalertmanagerconfig_webhook.go
@@ -34,7 +34,8 @@ func (r *VMAlertmanagerConfig) SetupWebhookWithManager(mgr ctrl.Manager) error {
 }
 
 func (amc *VMAlertmanagerConfig) Validate() error {
-	if err := parseNestedRoutes(amc.Spec.Route); err != nil {
+	validateSpec := amc.DeepCopy()
+	if err := parseNestedRoutes(validateSpec.Spec.Route); err != nil {
 		return fmt.Errorf("cannot parse nested route for alertmanager config err: %w", err)
 	}
 	return nil
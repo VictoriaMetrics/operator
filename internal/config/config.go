611819233bf595a4dbd04b07d7be24b7e994379c
f41gh7 <nik@victoriametrics.com>
2023-03-15 17:23:29 +0100

    config-reloader: allows proxy-protocol for config reloader

--- internal/config/config.go
+++ internal/config/config.go
@@ -47,7 +47,7 @@ type BaseOperatorConf struct {
 	UseCustomConfigReloader bool `default:"false"`
 	// container registry name prefix, e.g. docker.io
 	ContainerRegistry         string `default:""`
-	CustomConfigReloaderImage string `default:"victoriametrics/operator:config-reloader-v0.27.0"`
+	CustomConfigReloaderImage string `default:"victoriametrics/operator:config-reloader-v0.31.1"`
 	PSPAutoCreateEnabled      bool   `default:"true"`
 	VMAlertDefault            struct {
 		Image               string `default:"victoriametrics/vmalert"`

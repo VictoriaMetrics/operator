44af1ec8cbb757c18e1a375887a37e7d3d362047
Alexander Marshalov <_@marshalov.org>
2023-03-17 15:30:54 +0100

    fixed error with headers in VMAlertmanagerConfig.Receivers.EmailConfigs.Headers unmarshalling (#609)

--- api/v1beta1/vmalertmanagerconfig_test.go
+++ api/v1beta1/vmalertmanagerconfig_test.go
@@ -90,3 +90,89 @@ severity: labels.severity
 		Severity: "labels.severity",
 	})
 }
+
+func TestParsingEmailConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		spec    string
+		headers EmailConfigHeaders
+		wantErr bool
+	}{
+		{
+			name: "without headers",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+`,
+		},
+		{
+			name: "headers as map",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers:
+  X-My-Header: 'myValue'
+`,
+			headers: map[string]string{"X-My-Header": "myValue"},
+		},
+		{
+			name: "headers as map 2",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers:
+  key1: value1
+  key2: value2
+`,
+			headers: map[string]string{"key1": "value1", "key2": "value2"},
+		},
+		{
+			name: "headers as array",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers:
+  - key: X-My-Header
+    value: myValue
+`,
+			headers: map[string]string{"X-My-Header": "myValue"},
+		},
+		{
+			name: "headers as array 2",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers:
+  - key: key1
+    value: value1
+  - key: key2
+    value: value2
+`,
+			headers: map[string]string{"key1": "value1", "key2": "value2"},
+		},
+		{
+			name: "wrong type in headers",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers: 1
+`,
+			wantErr: true,
+		},
+		{
+			name: "wrong type in headers 2",
+			spec: `to: 'myAwesomeEmail@localhost.de'
+from: 'myOtherAwesomeEmail@localhost.de'
+headers: "headers"
+`,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var config EmailConfig
+			err := yaml.Unmarshal([]byte(tt.spec), &config)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("EmailConfigHeaders.UnmarshalYAML() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(config.Headers, tt.headers) {
+				t.Fatalf("EmailConfigHeaders.UnmarshalYAML() wrong enmarshalled headers, got: %v, want: %v", config.Headers, tt.headers)
+			}
+		})
+	}
+}

7c7af3e930e3b4fbbcffc9950420bfc8c49696f1
Haleygo <hui.wang@daocloud.io>
2023-08-03 22:58:41 +0800

    add default SeccompProfile and Capabilities

--- controllers/factory/builders.go
+++ controllers/factory/builders.go
@@ -534,6 +534,9 @@ func addStrictSecuritySettingsToPod(p *v1.PodSecurityContext, enableStrictSecuri
 		RunAsUser:  pointer.Int64(65534),
 		RunAsGroup: pointer.Int64(65534),
 		FSGroup:    pointer.Int64(65534),
+		SeccompProfile: &v1.SeccompProfile{
+			Type: v1.SeccompProfileTypeRuntimeDefault,
+		},
 	}
 	if k8stools.IsFSGroupChangePolicySupported() {
 		onRootMismatch := v1.FSGroupChangeOnRootMismatch
@@ -552,6 +555,11 @@ func addStrictSecuritySettingsToContainers(containers []v1.Container, enableStri
 			container.SecurityContext = &v1.SecurityContext{
 				ReadOnlyRootFilesystem:   pointer.Bool(true),
 				AllowPrivilegeEscalation: pointer.Bool(false),
+				Capabilities: &v1.Capabilities{
+					Drop: []v1.Capability{
+						"ALL",
+					},
+				},
 			}
 		}
 	}

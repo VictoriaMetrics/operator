c32931b568d63bba16fa672378ed33af2365d01a
Alexander Marshalov <_@marshalov.org>
2023-09-02 16:09:31 +0200

    prepare for release 1.37.1 (#743)

--- internal/config/config.go
+++ internal/config/config.go
@@ -53,7 +53,7 @@ type BaseOperatorConf struct {
 	PSPAutoCreateEnabled      bool   `default:"false"`
 	VMAlertDefault            struct {
 		Image               string `default:"victoriametrics/vmalert"`
-		Version             string `default:"v1.93.1"`
+		Version             string `default:"v1.93.3"`
 		Port                string `default:"8080"`
 		UseDefaultResources bool   `default:"true"`
 		Resource            struct {
@@ -72,7 +72,7 @@ type BaseOperatorConf struct {
 	}
 	VMAgentDefault struct {
 		Image               string `default:"victoriametrics/vmagent"`
-		Version             string `default:"v1.93.1"`
+		Version             string `default:"v1.93.3"`
 		ConfigReloadImage   string `default:"quay.io/prometheus-operator/prometheus-config-reloader:v0.58.0"`
 		Port                string `default:"8429"`
 		UseDefaultResources bool   `default:"true"`
@@ -92,7 +92,7 @@ type BaseOperatorConf struct {
 
 	VMSingleDefault struct {
 		Image               string `default:"victoriametrics/victoria-metrics"`
-		Version             string `default:"v1.93.1"`
+		Version             string `default:"v1.93.3"`
 		Port                string `default:"8429"`
 		UseDefaultResources bool   `default:"true"`
 		Resource            struct {
@@ -113,7 +113,7 @@ type BaseOperatorConf struct {
 		UseDefaultResources bool `default:"true"`
 		VMSelectDefault     struct {
 			Image    string `default:"victoriametrics/vmselect"`
-			Version  string `default:"v1.93.1-cluster"`
+			Version  string `default:"v1.93.3-cluster"`
 			Port     string `default:"8481"`
 			Resource struct {
 				Limit struct {
@@ -128,7 +128,7 @@ type BaseOperatorConf struct {
 		}
 		VMStorageDefault struct {
 			Image        string `default:"victoriametrics/vmstorage"`
-			Version      string `default:"v1.93.1-cluster"`
+			Version      string `default:"v1.93.3-cluster"`
 			VMInsertPort string `default:"8400"`
 			VMSelectPort string `default:"8401"`
 			Port         string `default:"8482"`
@@ -145,7 +145,7 @@ type BaseOperatorConf struct {
 		}
 		VMInsertDefault struct {
 			Image    string `default:"victoriametrics/vminsert"`
-			Version  string `default:"v1.93.1-cluster"`
+			Version  string `default:"v1.93.3-cluster"`
 			Port     string `default:"8480"`
 			Resource struct {
 				Limit struct {
@@ -183,7 +183,7 @@ type BaseOperatorConf struct {
 	DisableSelfServiceScrapeCreation bool `default:"false"`
 	VMBackup                         struct {
 		Image               string `default:"victoriametrics/vmbackupmanager"`
-		Version             string `default:"v1.93.1-enterprise"`
+		Version             string `default:"v1.93.3-enterprise"`
 		Port                string `default:"8300"`
 		UseDefaultResources bool   `default:"true"`
 		Resource            struct {
@@ -201,7 +201,7 @@ type BaseOperatorConf struct {
 	}
 	VMAuthDefault struct {
 		Image               string `default:"victoriametrics/vmauth"`
-		Version             string `default:"v1.93.1"`
+		Version             string `default:"v1.93.3"`
 		ConfigReloadImage   string `default:"quay.io/prometheus-operator/prometheus-config-reloader:v0.48.1"`
 		Port                string `default:"8427"`
 		UseDefaultResources bool   `default:"true"`

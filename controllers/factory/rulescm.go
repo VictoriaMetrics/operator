81ad4d78dd085ee8e6d09485f3a620999af99e7e
Timofei Kukushkin <t.kukushkin@cian.ru>
2023-09-18 20:01:43 +0400

    Remove another duplicate of `vmalert` log field

--- controllers/factory/rulescm.go
+++ controllers/factory/rulescm.go
@@ -85,9 +85,7 @@ func CreateOrUpdateRuleConfigMaps(ctx context.Context, cr *victoriametricsv1beta
 	}
 
 	if len(currentConfigMaps) == 0 {
-		l.Info("no Rule configmap found, creating new one", "namespace", cr.Namespace,
-			"vmalert", cr.Name,
-		)
+		l.Info("no Rule configmap found, creating new one", "namespace", cr.Namespace)
 		for _, cm := range newConfigMaps {
 			err := rclient.Create(ctx, &cm, &client.CreateOptions{})
 			if err != nil {

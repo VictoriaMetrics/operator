1c670efcff23b12a3ec69a360a9a2d5b514a14d3
f41gh7 <nik@victoriametrics.com>
2024-03-04 02:25:08 +0100

    removes flaky test

--- controllers/factory/k8stools/sts_test.go
+++ controllers/factory/k8stools/sts_test.go
@@ -221,12 +221,11 @@ func Test_performRollingUpdateOnSts(t *testing.T) {
 		c         *config.BaseOperatorConf
 	}
 	tests := []struct {
-		name                string
-		args                args
-		wantErr             bool
-		predefinedObjects   []runtime.Object
-		updatePodRevByIndex *int32
-		neededPodRev        string
+		name              string
+		args              args
+		wantErr           bool
+		predefinedObjects []runtime.Object
+		neededPodRev      string
 	}{
 		{
 			name: "rolling update is not needed",
@@ -310,87 +309,10 @@ func Test_performRollingUpdateOnSts(t *testing.T) {
 			},
 			wantErr: true,
 		},
-		{
-			name: "rolling update is needed with update",
-			args: args{
-				stsName: "vmselect-sts",
-				ns:      "default",
-				c: &config.BaseOperatorConf{
-					PodWaitReadyTimeout:       time.Second * 2,
-					PodWaitReadyInitDelay:     time.Millisecond,
-					PodWaitReadyIntervalCheck: time.Second,
-				},
-				podLabels: map[string]string{"app": "vmselect"},
-			},
-
-			predefinedObjects: []runtime.Object{
-				&appsv1.StatefulSet{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "vmselect-sts",
-						Namespace: "default",
-						Labels:    map[string]string{"app": "vmselect"},
-					},
-					Spec: appsv1.StatefulSetSpec{Replicas: pointer.Int32Ptr(2)},
-					Status: appsv1.StatefulSetStatus{
-						CurrentRevision: "rev1",
-						UpdateRevision:  "rev2",
-					},
-				},
-				&corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "vmselect-sts-0",
-						Namespace: "default",
-						Labels:    map[string]string{"app": "vmselect", podRevisionLabel: "rev1"},
-					},
-					Status: corev1.PodStatus{},
-				},
-				&corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "vmselect-sts-1",
-						Namespace: "default",
-						Labels:    map[string]string{"app": "vmselect", podRevisionLabel: "rev2"},
-					},
-					Status: corev1.PodStatus{
-						Phase: corev1.PodRunning,
-						Conditions: []corev1.PodCondition{
-							{
-								Type:   corev1.PodReady,
-								Status: "True",
-							},
-						},
-					},
-				},
-			},
-			updatePodRevByIndex: pointer.Int32Ptr(1),
-			neededPodRev:        "rev2",
-			wantErr:             false,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fclient := GetTestClientWithObjects(tt.predefinedObjects)
-			if tt.updatePodRevByIndex != nil {
-				podInd := tt.predefinedObjects[int(*tt.updatePodRevByIndex)]
-				pod := podInd.(*corev1.Pod)
-				go func(pod *corev1.Pod, rev string) {
-					time.Sleep(time.Millisecond * 1200)
-					pod.ObjectMeta.Labels[podRevisionLabel] = rev
-					pod.Status.Phase = corev1.PodRunning
-					pod.Status.Conditions = []corev1.PodCondition{
-						{
-							Status: "True",
-							Type:   corev1.PodReady,
-						},
-					}
-					err := fclient.Update(context.Background(), pod)
-					if err != nil {
-						t.Errorf("cannot update pod for rolling update check: %s", err)
-					}
-					if err := fclient.Status().Update(context.Background(), pod); err != nil {
-						t.Errorf("cannot update pod status for update check: %s", err)
-					}
-				}(pod, tt.neededPodRev)
-			}
 
 			if err := performRollingUpdateOnSts(context.Background(), false, fclient, tt.args.stsName, tt.args.ns, tt.args.podLabels, tt.args.c); (err != nil) != tt.wantErr {
 				t.Errorf("performRollingUpdateOnSts() error = %v, wantErr %v", err, tt.wantErr)

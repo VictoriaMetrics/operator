db5cc3e9208cb24fc289236e253b123b06ae7994
f41gh7 <nik@victoriametrics.com>
2024-09-10 00:59:07 +0200

    controllers: adds period resync for vmcluster and vmsingle
    It allows to properly track live status of pods
    
    Signed-off-by: f41gh7 <nik@victoriametrics.com>

--- internal/controller/operator/vmcluster_controller.go
+++ internal/controller/operator/vmcluster_controller.go
@@ -64,13 +64,20 @@ func (r *VMClusterReconciler) Reconcile(ctx context.Context, request ctrl.Reques
 	if err := finalize.AddFinalizer(ctx, r.Client, instance); err != nil {
 		return result, err
 	}
-	return reconcileAndTrackStatus(ctx, r.Client, instance, func() (ctrl.Result, error) {
+	result, err = reconcileAndTrackStatus(ctx, r.Client, instance, func() (ctrl.Result, error) {
 		err = vmcluster.CreateOrUpdateVMCluster(ctx, instance, r.Client, config.MustGetBaseConfig())
 		if err != nil {
 			return result, fmt.Errorf("failed create or update vmcluster: %w", err)
 		}
 		return result, nil
 	})
+	if err != nil {
+		return
+	}
+	if r.BaseConf.ForceResyncInterval > 0 {
+		result.RequeueAfter = r.BaseConf.ForceResyncInterval
+	}
+	return
 }
 
 // SetupWithManager general setup method

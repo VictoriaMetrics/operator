ab9f2b8e59d56cdacfc4395682b4b6f7dd6ecbec
Zakhar Bessarab <z.bessarab@victoriametrics.com>
2025-06-09 17:02:45 +0400

    config: update versions of components to latest ones
     - VictoriaMetrics to 1.119.0
     - VictoriaLogs to 1.23.3

--- internal/config/config.go
+++ internal/config/config.go
@@ -30,8 +30,8 @@ var (
 	//
 	// DO NOT FORGET TO MODIFY VERSIONS IN BaseOperatorConf
 	defaultEnvs = map[string]string{
-		"VM_METRICS_VERSION": "v1.118.0",
-		"VM_LOGS_VERSION":    "v1.21.0",
+		"VM_METRICS_VERSION": "v1.119.0",
+		"VM_LOGS_VERSION":    "v1.23.3",
 	}
 )
 
@@ -97,8 +97,8 @@ type BaseOperatorConf struct {
 	//
 	// DO NOT FORGET TO MODIFY VERSIONS IN defaultEnvs
 
-	MetricsVersion string `default:"v1.118.0" env:"METRICS_VERSION"`
-	LogsVersion    string `default:"v1.21.0" env:"LOGS_VERSION"`
+	MetricsVersion string `default:"v1.119.0" env:"METRICS_VERSION"`
+	LogsVersion    string `default:"v1.23.3" env:"LOGS_VERSION"`
 
 	// enables custom config reloader for vmauth and vmagent,
 	// it should speed-up config reloading process.

VictoriaMetrics/operator

commit b7201a0bb156c5cf1c47f4f35ef5cd2a635a19b1

operator: adds new validation webhooks
 This commit adds the following changes:

* webhooks: add validation webhooks for VMServiceScrape,VMPodScrape,VMNodeScrape,VMProbe,VMStaticScrape and VMScrapeConfig objects.
  Currently, it only performs parsing validation. But later it'll be extended with logical checks and proper relabeling rules valdation.
* api: introduce ParsingError field for Scrape Objects
   It must provide better feed back for users on schemaless fields.
  Such as `regex` at relabeling config, which may accept various types.

Related issues:
https://github.com/VictoriaMetrics/operator/issues/1085
https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8426
https://github.com/VictoriaMetrics/operator/issues/1329
---------

Co-authored-by: f41gh7 <nik@victoriametrics.com>

api/operator/v1beta1/vmpodscrape_types.go
@@ -1,14 +1,19 @@
 package v1beta1
 
 import (
+	"encoding/json"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
+var _ json.Unmarshaler = (*VMPodScrapeSpec)(nil)
+
 // VMPodScrapeSpec defines the desired state of VMPodScrape
 type VMPodScrapeSpec struct {
+	// ParsingError contents error with context if operator was failed to parse json object from kubernetes api server
+	ParsingError string `json:"-" yaml:"-"`
 	// The label to use to retrieve the job name from.
 	// +optional
 	JobLabel string `json:"jobLabel,omitempty"`
@@ -36,6 +41,16 @@ type VMPodScrapeSpec struct {
 	// AttachMetadata configures metadata attaching from service discovery
 	// +optional
 	AttachMetadata AttachMetadata `json:"attach_metadata,omitempty"`
+}
+
+// UnmarshalJSON implements json.Unmarshaler interface
+func (cr *VMPodScrapeSpec) UnmarshalJSON(src []byte) error {
+	type pcr VMPodScrapeSpec
+	if err := json.Unmarshal(src, (*pcr)(cr)); err != nil {
+		cr.ParsingError = fmt.Sprintf("cannot parse spec: %s, err: %s", string(src), err)
+		return nil
+	}
+	return nil
 }
 
 // VMPodScrape is scrape configuration for pods,
@@ -106,6 +121,19 @@ type PodMetricsEndpoint struct {
 // attack, users can instead use the BearerTokenSecret field.
 type ArbitraryFSAccessThroughSMsConfig struct {
 	Deny bool `json:"deny,omitempty"`
+}
+
+// Validate returns error if CR is invalid
+func (cr *VMPodScrape) Validate() error {
+	if mustSkipValidation(cr) {
+		return nil
+	}
+	for _, endpoint := range cr.Spec.PodMetricsEndpoints {
+		if err := endpoint.EndpointRelabelings.validate(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // AsProxyKey builds key for proxy cache maps

api/operator/v1beta1/vmprobe_types.go
@@ -17,14 +17,19 @@ limitations under the License.
 package v1beta1
 
 import (
+	"encoding/json"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+var _ json.Unmarshaler = (*VMProbeSpec)(nil)
+
 // VMProbeSpec contains specification parameters for a Probe.
 // +k8s:openapi-gen=true
 type VMProbeSpec struct {
+	// ParsingError contents error with context if operator was failed to parse json object from kubernetes api server
+	ParsingError string `json:"-" yaml:"-"`
 	// The job name assigned to scraped metrics by default.
 	JobName string `json:"jobName,omitempty"`
 	// Specification for the prober to use for probing targets.
@@ -42,6 +47,16 @@ type VMProbeSpec struct {
 
 	EndpointAuth         `json:",inline"`
 	EndpointScrapeParams `json:",inline"`
+}
+
+// UnmarshalJSON implements json.Unmarshaler interface
+func (cr *VMProbeSpec) UnmarshalJSON(src []byte) error {
+	type pcr VMProbeSpec
+	if err := json.Unmarshal(src, (*pcr)(cr)); err != nil {
+		cr.ParsingError = fmt.Sprintf("cannot parse spec: %s, err: %s", string(src), err)
+		return nil
+	}
+	return nil
 }
 
 // VMProbeTargets defines a set of static and dynamically discovered targets for the prober.
@@ -130,6 +145,27 @@ func (cr *VMProbe) AsMapKey() string {
 // GetStatusMetadata implements reconcile.objectWithStatus interface
 func (cr *VMProbe) GetStatusMetadata() *StatusMetadata {
 	return &cr.Status.StatusMetadata
+}
+
+// Validate returns error if CR is invalid
+func (cr *VMProbe) Validate() error {
+	if mustSkipValidation(cr) {
+		return nil
+	}
+	if err := checkRelabelConfigs(cr.Spec.MetricRelabelConfigs); err != nil {
+		return fmt.Errorf("invalid metricRelabelConfigs: %w", err)
+	}
+	switch {
+	case cr.Spec.Targets.Ingress != nil:
+		if err := checkRelabelConfigs(cr.Spec.Targets.Ingress.RelabelConfigs); err != nil {
+			return fmt.Errorf("invliad ingress.relabelingConfigs: %w", err)
+		}
+	case cr.Spec.Targets.StaticConfig != nil:
+		if err := checkRelabelConfigs(cr.Spec.Targets.StaticConfig.RelabelConfigs); err != nil {
+			return fmt.Errorf("invliad staticConfig.relabelingConfigs: %w", err)
+		}
+	}
+	return nil
 }
 
 func init() {

api/operator/v1beta1/vmservicescrape_types.go
@@ -1,14 +1,19 @@
 package v1beta1
 
 import (
+	"encoding/json"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
+var _ json.Unmarshaler = (*VMServiceScrapeSpec)(nil)
+
 // VMServiceScrapeSpec defines the desired state of VMServiceScrape
 type VMServiceScrapeSpec struct {
+	// ParsingError contents error with context if operator was failed to parse json object from kubernetes api server
+	ParsingError string `json:"-" yaml:"-"`
 	// DiscoveryRole - defines kubernetes_sd role for objects discovery.
 	// by default, its endpoints.
 	// can be changed to service or endpointslices.
@@ -47,6 +52,16 @@ type VMServiceScrapeSpec struct {
 	// AttachMetadata configures metadata attaching from service discovery
 	// +optional
 	AttachMetadata AttachMetadata `json:"attach_metadata,omitempty"`
+}
+
+// UnmarshalJSON implements json.Unmarshaler interface
+func (cr *VMServiceScrapeSpec) UnmarshalJSON(src []byte) error {
+	type pcr VMServiceScrapeSpec
+	if err := json.Unmarshal(src, (*pcr)(cr)); err != nil {
+		cr.ParsingError = fmt.Sprintf("cannot parse spec: %s, err: %s", string(src), err)
+		return nil
+	}
+	return nil
 }
 
 // VMServiceScrape is scrape configuration for endpoints associated with
@@ -133,6 +148,19 @@ type Endpoint struct {
 // AsProxyKey builds key for proxy cache maps
 func (cr *VMServiceScrape) AsProxyKey(i int) string {
 	return fmt.Sprintf("serviceScrapeProxy/%s/%s/%d", cr.Namespace, cr.Name, i)
+}
+
+// Validate returns error if CR is invalid
+func (cr *VMServiceScrape) Validate() error {
+	if mustSkipValidation(cr) {
+		return nil
+	}
+	for _, endpoint := range cr.Spec.Endpoints {
+		if err := endpoint.EndpointRelabelings.validate(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // AsMapKey - returns cr name with suffix for token/auth maps.

internal/controller/operator/factory/vmagent/vmagent_scrapeconfig.go
@@ -66,7 +66,7 @@ type scrapeObjects struct {
 	totalBrokenCount int
 }
 
-var skipAnyError = func(_ error) bool {
+var skipNon = func(_ error) bool {
 	return false
 }
 
@@ -84,7 +84,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 			return err
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for ServiceScrape: %s", err))
 	}
@@ -101,7 +101,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 			return err
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for PodScrape: %s", err))
 	}
@@ -115,7 +115,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 			}
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for StaticScrape: %s", err))
 	}
@@ -127,7 +127,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 			}
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for NodeScrape: %s", err))
 	}
@@ -145,7 +145,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 			}
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for ProbeScrape: %s", err))
 	}
@@ -159,7 +159,7 @@ func (so *scrapeObjects) mustValidateObjects(vmagentCR *vmv1beta1.VMAgent) {
 
 		}
 		return nil
-	}, skipAnyError)
+	}, skipNon)
 	if err != nil {
 		panic(fmt.Errorf("BUG: validation cannot return error for scrapeConfig: %s", err))
 	}
@@ -395,10 +395,12 @@ type scrapeObjectWithStatus interface {
 	GetStatusMetadata() *vmv1beta1.StatusMetadata
 }
 
-var skipOnNotFound = func(err error) bool {
+var skipOnNotFoundOrParseErr = func(err error) bool {
 	var ne *k8stools.KeyNotFoundError
 	switch {
 	case errors.IsNotFound(err), stderrors.As(err, &ne):
+		return true
+	case err != nil && strings.Contains(err.Error(), "cannot parse spec:"):
 		return true
 	default:
 		return false
@@ -486,6 +488,10 @@ func loadScrapeSecrets(
 	}
 	var err error
 	sos.sss, sos.sssBroken, err = forEachCollectSkipOn(sos.sss, sos.sssBroken, func(mon *vmv1beta1.VMServiceScrape) error {
+		if mon.Spec.ParsingError != "" {
+			return stderrors.New(mon.Spec.ParsingError)
+		}
+
 		for i, ep := range mon.Spec.Endpoints {
 			if err := loadSecretsToCacheFrom(ctx, rclient, &ep.EndpointAuth, mon.AsMapKey(i), mon.Namespace, ssCache); err != nil {
 				return err
@@ -506,12 +512,16 @@ func loadScrapeSecrets(
 			}
 		}
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
 
 	sos.nss, sos.nssBroken, err = forEachCollectSkipOn(sos.nss, sos.nssBroken, func(node *vmv1beta1.VMNodeScrape) error {
+		if node.Spec.ParsingError != "" {
+			return stderrors.New(node.Spec.ParsingError)
+		}
+
 		if err := loadSecretsToCacheFrom(ctx, rclient, &node.Spec.EndpointAuth, node.AsMapKey(), node.Namespace, ssCache); err != nil {
 			return err
 		}
@@ -530,12 +540,16 @@ func loadScrapeSecrets(
 		}
 
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
 
 	sos.pss, sos.pssBroken, err = forEachCollectSkipOn(sos.pss, sos.pssBroken, func(pod *vmv1beta1.VMPodScrape) error {
+		if pod.Spec.ParsingError != "" {
+			return stderrors.New(pod.Spec.ParsingError)
+		}
+
 		for i, ep := range pod.Spec.PodMetricsEndpoints {
 			if err := loadSecretsToCacheFrom(ctx, rclient, &ep.EndpointAuth, pod.AsMapKey(i), pod.Namespace, ssCache); err != nil {
 				return err
@@ -556,12 +570,16 @@ func loadScrapeSecrets(
 		}
 
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
 
 	sos.prss, sos.prssBroken, err = forEachCollectSkipOn(sos.prss, sos.prssBroken, func(probe *vmv1beta1.VMProbe) error {
+		if probe.Spec.ParsingError != "" {
+			return stderrors.New(probe.Spec.ParsingError)
+		}
+
 		if err := loadSecretsToCacheFrom(ctx, rclient, &probe.Spec.EndpointAuth, probe.AsMapKey(), probe.Namespace, ssCache); err != nil {
 			return err
 		}
@@ -579,12 +597,16 @@ func loadScrapeSecrets(
 			}
 		}
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
 
 	sos.stss, sos.stssBroken, err = forEachCollectSkipOn(sos.stss, sos.stssBroken, func(staticCfg *vmv1beta1.VMStaticScrape) error {
+		if staticCfg.Spec.ParsingError != "" {
+			return stderrors.New(staticCfg.Spec.ParsingError)
+		}
+
 		for i, ep := range staticCfg.Spec.TargetEndpoints {
 			if err := loadSecretsToCacheFrom(ctx, rclient, &ep.EndpointAuth, staticCfg.AsMapKey(i), staticCfg.Namespace, ssCache); err != nil {
 				return err
@@ -605,12 +627,16 @@ func loadScrapeSecrets(
 			}
 		}
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
 
 	sos.scss, sos.scssBroken, err = forEachCollectSkipOn(sos.scss, sos.scssBroken, func(scrapeConfig *vmv1beta1.VMScrapeConfig) error {
+		if scrapeConfig.Spec.ParsingError != "" {
+			return stderrors.New(scrapeConfig.Spec.ParsingError)
+		}
+
 		if err := loadSecretsToCacheFrom(ctx, rclient, &scrapeConfig.Spec.EndpointAuth, scrapeConfig.AsMapKey("", 0), scrapeConfig.Namespace, ssCache); err != nil {
 			return err
 		}
@@ -795,7 +821,7 @@ func loadScrapeSecrets(
 		}
 
 		return nil
-	}, skipOnNotFound)
+	}, skipOnNotFoundOrParseErr)
 	if err != nil {
 		return nil, err
 	}
@@ -1288,9 +1314,9 @@ func addTLStoYaml(cfg yaml.MapSlice, namespace string, tls *vmv1beta1.TLSConfig,
 	return cfg
 }
 
-func addRelabelConfigs(dst []yaml.MapSlice, rcs []vmv1beta1.RelabelConfig) []yaml.MapSlice {
+func addRelabelConfigs(dst []yaml.MapSlice, rcs []*vmv1beta1.RelabelConfig) []yaml.MapSlice {
 	for i := range rcs {
-		rc := &rcs[i]
+		rc := rcs[i]
 		if rc.IsEmpty() {
 			continue
 		}

PROJECT
@@ -66,6 +66,9 @@ resources:
   kind: VMPodScrape
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true
@@ -87,6 +90,9 @@ resources:
   kind: VMServiceScrape
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true
@@ -120,6 +126,9 @@ resources:
   kind: VMProbe
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true
@@ -129,6 +138,9 @@ resources:
   kind: VMNodeScrape
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true
@@ -138,6 +150,9 @@ resources:
   kind: VMStaticScrape
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true
@@ -171,6 +186,9 @@ resources:
   kind: VMScrapeConfig
   path: github.com/VictoriaMetrics/operator/api/operator/v1beta1
   version: v1beta1
+  webhooks:
+    validation: true
+    webhookVersion: v1
 - api:
     crdVersion: v1
     namespaced: true

api/operator/v1beta1/common_scrapeparams.go
@@ -321,3 +321,13 @@ type EndpointRelabelings struct {
 	// +optional
 	RelabelConfigs []*RelabelConfig `json:"relabelConfigs,omitempty"`
 }
+
+func (r *EndpointRelabelings) validate() error {
+	if err := checkRelabelConfigs(r.RelabelConfigs); err != nil {
+		return fmt.Errorf("invalid relabelConfigs: %w", err)
+	}
+	if err := checkRelabelConfigs(r.MetricRelabelConfigs); err != nil {
+		return fmt.Errorf("invalid metricRelabelConfigs: %w", err)
+	}
+	return nil
+}

api/operator/v1beta1/vmagent_types.go
@@ -112,7 +112,7 @@ type VMAgentSpec struct {
 	RelabelConfig *v1.ConfigMapKeySelector `json:"relabelConfig,omitempty"`
 	// InlineRelabelConfig - defines GlobalRelabelConfig for vmagent, can be defined directly at CRD.
 	// +optional
-	InlineRelabelConfig []RelabelConfig `json:"inlineRelabelConfig,omitempty"`
+	InlineRelabelConfig []*RelabelConfig `json:"inlineRelabelConfig,omitempty"`
 	// StreamAggrConfig defines global stream aggregation configuration for VMAgent
 	// +optional
 	StreamAggrConfig *StreamAggrConfig `json:"streamAggrConfig,omitempty"`
@@ -457,7 +457,7 @@ type VMAgentRemoteWriteSpec struct {
 	UrlRelabelConfig *v1.ConfigMapKeySelector `json:"urlRelabelConfig,omitempty"`
 	// InlineUrlRelabelConfig defines relabeling config for remoteWriteURL, it can be defined at crd spec.
 	// +optional
-	InlineUrlRelabelConfig []RelabelConfig `json:"inlineUrlRelabelConfig,omitempty"`
+	InlineUrlRelabelConfig []*RelabelConfig `json:"inlineUrlRelabelConfig,omitempty"`
 	// OAuth2 defines auth configuration
 	// +optional
 	OAuth2 *OAuth2 `json:"oauth2,omitempty"`
@@ -875,7 +875,7 @@ func (cr *VMAgent) GetAdditionalService() *AdditionalServiceSpec {
 	return cr.Spec.ServiceSpec
 }
 
-func checkRelabelConfigs(src []RelabelConfig) error {
+func checkRelabelConfigs(src []*RelabelConfig) error {
 	// TODO: restore check when issue will be fixed at golang
 	// https://github.com/VictoriaMetrics/VictoriaMetrics/issues/6911
 	return nil

api/operator/v1beta1/vmagent_types_test.go
@@ -42,7 +42,7 @@ func TestVMAgent_Validate(t *testing.T) {
 			name: "valid relabeling",
 			spec: VMAgentSpec{
 				RemoteWrite: []VMAgentRemoteWriteSpec{{URL: "http://some-rw"}},
-				InlineRelabelConfig: []RelabelConfig{
+				InlineRelabelConfig: []*RelabelConfig{
 					{
 						Action:       "drop",
 						SourceLabels: []string{"src_id"},
@@ -54,7 +54,7 @@ func TestVMAgent_Validate(t *testing.T) {
 			name: "relabeling with if array",
 			spec: VMAgentSpec{
 				RemoteWrite: []VMAgentRemoteWriteSpec{{URL: "http://some-rw"}},
-				InlineRelabelConfig: []RelabelConfig{
+				InlineRelabelConfig: []*RelabelConfig{
 					{
 						Action: "drop_metrics",
 						If: []string{

commit 06288fb1f1a37d204ac2f21f2076046cacc85bde

build/vmagent: properly validate scrape objects
 Previous commit b7201a0bb156c5cf1c47f4f35ef5cd2a635a19b1 introduced
regression at scrape objects syntax validation.
 It incorrectly returned error in case, when scrape object supposed to
 be marked as invalid and excluded from config generation.

  This commit restores original behaviour and adds tests to validate it.

Fixes https://github.com/VictoriaMetrics/operator/issues/1415

api/operator/v1beta1/vmpodscrape_types.go
@@ -133,6 +133,9 @@ func (cr *VMPodScrape) Validate() error {
 			return err
 		}
 	}
+	if _, err := metav1.LabelSelectorAsSelector(&cr.Spec.Selector); err != nil {
+		return err
+	}
 	return nil
 }
 

api/operator/v1beta1/vmprobe_types.go
@@ -148,6 +148,9 @@ func (cr *VMProbe) Validate() error {
 	}
 	switch {
 	case cr.Spec.Targets.Ingress != nil:
+		if _, err := metav1.LabelSelectorAsSelector(&cr.Spec.Targets.Ingress.Selector); err != nil {
+			return fmt.Errorf("invalid spec.targets.ingress.selector syntax: %w", err)
+		}
 		if err := checkRelabelConfigs(cr.Spec.Targets.Ingress.RelabelConfigs); err != nil {
 			return fmt.Errorf("invalid ingress.relabelingConfigs: %w", err)
 		}

api/operator/v1beta1/vmservicescrape_types.go
@@ -155,6 +155,9 @@ func (cr *VMServiceScrape) Validate() error {
 			return err
 		}
 	}
+	if _, err := metav1.LabelSelectorAsSelector(&cr.Spec.Selector); err != nil {
+		return err
+	}
 	return nil
 }
 

internal/controller/operator/factory/vmagent/vmagent_scrapeconfig.go
@@ -38,13 +38,8 @@ type scrapeObjects struct {
 	scssBroken []*vmv1beta1.VMScrapeConfig
 }
 
-var skipNon = func(_ error) bool {
-	return false
-}
-
-func (so *scrapeObjects) mustValidateObjects(cr *vmv1beta1.VMAgent) {
-	var err error
-	so.sss, so.sssBroken, err = forEachCollectSkipOn(so.sss, so.sssBroken, func(ss *vmv1beta1.VMServiceScrape) error {
+func (so *scrapeObjects) validateObjects(cr *vmv1beta1.VMAgent) {
+	so.sss, so.sssBroken = forEachCollectSkipInvalid(so.sss, so.sssBroken, func(ss *vmv1beta1.VMServiceScrape) error {
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			for _, ep := range ss.Spec.Endpoints {
 				if err := testForArbitraryFSAccess(ep.EndpointAuth); err != nil {
@@ -52,16 +47,13 @@ func (so *scrapeObjects) mustValidateObjects(cr *vmv1beta1.VMAgent) {
 				}
 			}
 		}
-		if _, err := metav1.LabelSelectorAsSelector(&ss.Spec.Selector); err != nil {
+		if err := ss.Validate(); err != nil {
 			return err
 		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for ServiceScrape: %s", err))
-	}
+	})
 
-	so.pss, so.pssBroken, err = forEachCollectSkipOn(so.pss, so.pssBroken, func(ps *vmv1beta1.VMPodScrape) error {
+	so.pss, so.pssBroken = forEachCollectSkipInvalid(so.pss, so.pssBroken, func(ps *vmv1beta1.VMPodScrape) error {
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			for _, ep := range ps.Spec.PodMetricsEndpoints {
 				if err := testForArbitraryFSAccess(ep.EndpointAuth); err != nil {
@@ -69,16 +61,12 @@ func (so *scrapeObjects) mustValidateObjects(cr *vmv1beta1.VMAgent) {
 				}
 			}
 		}
-		if _, err := metav1.LabelSelectorAsSelector(&ps.Spec.Selector); err != nil {
+		if err := ps.Validate(); err != nil {
 			return err
 		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for PodScrape: %s", err))
-	}
-
-	so.stss, so.stssBroken, err = forEachCollectSkipOn(so.stss, so.stssBroken, func(sts *vmv1beta1.VMStaticScrape) error {
+	})
+	so.stss, so.stssBroken = forEachCollectSkipInvalid(so.stss, so.stssBroken, func(sts *vmv1beta1.VMStaticScrape) error {
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			for _, ep := range sts.Spec.TargetEndpoints {
 				if err := testForArbitraryFSAccess(ep.EndpointAuth); err != nil {
@@ -86,55 +74,45 @@ func (so *scrapeObjects) mustValidateObjects(cr *vmv1beta1.VMAgent) {
 				}
 			}
 		}
+		if err := sts.Validate(); err != nil {
+			return err
+		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for StaticScrape: %s", err))
-	}
+	})
 
-	so.nss, so.nssBroken, err = forEachCollectSkipOn(so.nss, so.nssBroken, func(ns *vmv1beta1.VMNodeScrape) error {
+	so.nss, so.nssBroken = forEachCollectSkipInvalid(so.nss, so.nssBroken, func(ns *vmv1beta1.VMNodeScrape) error {
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			if err := testForArbitraryFSAccess(ns.Spec.EndpointAuth); err != nil {
 				return err
 			}
 		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for NodeScrape: %s", err))
-	}
+	})
 
-	so.prss, so.prssBroken, err = forEachCollectSkipOn(so.prss, so.prssBroken, func(prs *vmv1beta1.VMProbe) error {
+	so.prss, so.prssBroken = forEachCollectSkipInvalid(so.prss, so.prssBroken, func(prs *vmv1beta1.VMProbe) error {
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			if err := testForArbitraryFSAccess(prs.Spec.EndpointAuth); err != nil {
 				return err
 			}
 		}
-		if prs.Spec.Targets.Ingress != nil {
-			_, err := metav1.LabelSelectorAsSelector(&prs.Spec.Targets.Ingress.Selector)
-			if err != nil {
-				return fmt.Errorf("cannot parse spec.selector: %w", err)
-			}
+		if err := prs.Validate(); err != nil {
+			return err
 		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for ProbeScrape: %s", err))
-	}
+	})
 
-	so.scss, so.scssBroken, err = forEachCollectSkipOn(so.scss, so.scssBroken, func(scss *vmv1beta1.VMScrapeConfig) error {
+	so.scss, so.scssBroken = forEachCollectSkipInvalid(so.scss, so.scssBroken, func(scss *vmv1beta1.VMScrapeConfig) error {
 		// TODO: @f41gh7 validate per configuration FS access
 		if cr.Spec.ArbitraryFSAccessThroughSMs.Deny {
 			if err := testForArbitraryFSAccess(scss.Spec.EndpointAuth); err != nil {
 				return err
 			}
-
+		}
+		if err := scss.Validate(); err != nil {
+			return err
 		}
 		return nil
-	}, skipNon)
-	if err != nil {
-		panic(fmt.Errorf("BUG: validation cannot return error for scrapeConfig: %s", err))
-	}
+	})
 }
 
 // CreateOrUpdateConfigurationSecret builds scrape configuration for VMAgent
@@ -198,7 +176,7 @@ func createOrUpdateConfigurationSecret(ctx context.Context, rclient client.Clien
 		stss: statics,
 		scss: scrapeConfigs,
 	}
-	sos.mustValidateObjects(cr)
+	sos.validateObjects(cr)
 
 	var additionalScrapeConfigs []byte
 
@@ -371,23 +349,20 @@ type scrapeObjectWithStatus interface {
 }
 
 // returned objects with not found links have erased type
-func forEachCollectSkipOn[T scrapeObjectWithStatus](src, srcBroken []T, apply func(s T) error, skipOn func(error) bool) ([]T, []T, error) {
+func forEachCollectSkipInvalid[T scrapeObjectWithStatus](src, srcBroken []T, apply func(s T) error) ([]T, []T) {
 	var cnt int
 	for _, o := range src {
 		if err := apply(o); err != nil {
-			if skipOn(err) {
-				st := o.GetStatusMetadata()
-				st.CurrentSyncError = err.Error()
-				srcBroken = append(srcBroken, o)
-				continue
-			}
-			return nil, nil, err
+			st := o.GetStatusMetadata()
+			st.CurrentSyncError = err.Error()
+			srcBroken = append(srcBroken, o)
+			continue
 		}
 		src[cnt] = o
 		cnt++
 	}
 	src = src[:cnt]
-	return src, srcBroken, nil
+	return src, srcBroken
 }
 
 // TODO: @f41gh7 validate VMScrapeParams

internal/controller/operator/factory/vmagent/vmagent_scrapeconfig_test.go
@@ -1597,6 +1597,80 @@ scrape_configs:
     replacement: default/pod-1
 `,
 		},
+		{
+			name: "with invalid objects syntax",
+			cr: &vmv1beta1.VMAgent{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "select-all",
+					Namespace: "default",
+				},
+				Spec: vmv1beta1.VMAgentSpec{
+					SelectAllByDefault: true,
+					VMAgentSecurityEnforcements: vmv1beta1.VMAgentSecurityEnforcements{
+						ArbitraryFSAccessThroughSMs: vmv1beta1.ArbitraryFSAccessThroughSMsConfig{
+							Deny: true,
+						},
+					},
+					RemoteWrite: []vmv1beta1.VMAgentRemoteWriteSpec{
+						{
+							URL: "http://some-single.example.com",
+						},
+					},
+				},
+			},
+			predefinedObjects: []runtime.Object{
+				&vmv1beta1.VMServiceScrape{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Name:      "fs-access",
+					},
+					Spec: vmv1beta1.VMServiceScrapeSpec{
+						Endpoints: []vmv1beta1.Endpoint{
+							{
+								Port: "8080",
+								EndpointAuth: vmv1beta1.EndpointAuth{
+									TLSConfig: &vmv1beta1.TLSConfig{
+										CAFile: "/etc/passwd",
+									},
+								},
+							},
+						},
+					},
+				},
+				&vmv1beta1.VMPodScrape{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Name:      "bad-syntax",
+					},
+					Spec: vmv1beta1.VMPodScrapeSpec{
+						Selector: *metav1.SetAsLabelSelector(map[string]string{
+							"alb.ingress.kubernetes.io/tags": "Environment=devl",
+						}),
+					},
+				},
+				&vmv1beta1.VMProbe{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Name:      "bad-syntax",
+					},
+					Spec: vmv1beta1.VMProbeSpec{
+						Targets: vmv1beta1.VMProbeTargets{
+							Ingress: &vmv1beta1.ProbeTargetIngress{
+								Selector: *metav1.SetAsLabelSelector(map[string]string{
+									"alb.ingress.kubernetes.io/tags": "Environment=devl",
+								}),
+							},
+						},
+					},
+				},
+			},
+			wantConfig: `global:
+  scrape_interval: 30s
+  external_labels:
+    prometheus: default/select-all
+scrape_configs: []
+`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -1630,7 +1704,6 @@ scrape_configs:
 				t.Fatalf("cannot read cfg: %s", err)
 			}
 			gr.Close()
-
 			assert.Equal(t, tt.wantConfig, string(data))
 		})
 	}
67a021830fec0f8102d5e4d359bc627f8bf45366
f41gh7 <nik@victoriametrics.com>
2025-07-11 15:19:05 +0200

    add more review comments
    * properly apply on delete for vlagent
    * restore vlcluster tag for e2e tests
    
    Signed-off-by: f41gh7 <nik@victoriametrics.com>

--- internal/controller/operator/factory/finalize/vlagent.go
+++ internal/controller/operator/factory/finalize/vlagent.go
@@ -8,26 +8,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	vmv1 "github.com/VictoriaMetrics/operator/api/operator/v1"
-	"github.com/VictoriaMetrics/operator/internal/controller/operator/factory/build"
 )
 
 // OnVLAgentDelete deletes all vlagent related resources
 func OnVLAgentDelete(ctx context.Context, rclient client.Client, cr *vmv1.VLAgent) error {
-	// check deployment
-	if err := removeFinalizeObjByName(ctx, rclient, &appsv1.Deployment{}, cr.PrefixedName(), cr.Namespace); err != nil {
-		return err
-	}
 	if err := removeFinalizeObjByName(ctx, rclient, &appsv1.StatefulSet{}, cr.PrefixedName(), cr.Namespace); err != nil {
-		return err
-	}
-	if err := removeFinalizeObjByName(ctx, rclient, &appsv1.DaemonSet{}, cr.PrefixedName(), cr.Namespace); err != nil {
-		return err
-	}
-
-	if err := RemoveOrphanedDeployments(ctx, rclient, cr, nil); err != nil {
-		return err
-	}
-	if err := RemoveOrphanedSTSs(ctx, rclient, cr, nil); err != nil {
 		return err
 	}
 	// check service
@@ -38,15 +23,6 @@ func OnVLAgentDelete(ctx context.Context, rclient client.Client, cr *vmv1.VLAgen
 		if err := removeFinalizeObjByName(ctx, rclient, &corev1.Service{}, cr.Spec.ServiceSpec.NameOrDefault(cr.PrefixedName()), cr.Namespace); err != nil {
 			return err
 		}
-	}
-	// config secret
-	if err := removeFinalizeObjByName(ctx, rclient, &corev1.Secret{}, cr.PrefixedName(), cr.Namespace); err != nil {
-		return err
-	}
-
-	// check secret for tls assets
-	if err := removeFinalizeObjByName(ctx, rclient, &corev1.Secret{}, build.ResourceName(build.TLSAssetsResourceKind, cr), cr.Namespace); err != nil {
-		return err
 	}
 
 	// check PDB

39d12544d71f7ebdc442335d49809e672f1d4a27
Vadim Rutkovsky <roignac@gmail.com>
2025-10-27 13:49:26 +0100

    Add test cases for update and error handling in reconcileInlineVMCluster.

--- operator/internal/controller/operator/factory/vmdistributedcluster/reconcileinlinevmcluster_test.go
+++ operator/internal/controller/operator/factory/vmdistributedcluster/reconcileinlinevmcluster_test.go
@@ -76,6 +76,93 @@ func Test_reconcileInlineVMCluster(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "Successfully updates an existing VMCluster",
+			args: args{
+				crName:    "test-vmdc",
+				namespace: "default",
+				index:     0,
+				refOrSpec: vmv1alpha1.VMClusterRefOrSpec{
+					Name: "inline-vmcluster",
+					VMClusterSpec: &vmv1beta1.VMClusterSpec{
+						Image: "victoriametrics/vmcluster:new-version",
+					},
+				},
+			},
+			existingObjs: []runtime.Object{
+				&vmv1beta1.VMCluster{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-vmdc-inline-vmcluster",
+						Namespace: "default",
+						Labels: map[string]string{
+							"app.kubernetes.io/managed-by": "vmdistributedcluster",
+							"app.kubernetes.io/instance":   "test-vmdc",
+						},
+						Annotations: map[string]string{
+							"vmdistributedcluster.victoriametrics.com/inline-index": "0",
+						},
+						OwnerReferences: []metav1.OwnerReference{
+							{
+								APIVersion: "operator.victoriametrics.com/v1alpha1",
+								Kind:       "VMDistributedCluster",
+								Name:       "test-vmdc",
+							},
+						},
+					},
+					Spec: vmv1beta1.VMClusterSpec{
+						Image: "victoriametrics/vmcluster:old-version",
+					},
+				},
+			},
+			want: &vmv1beta1.VMCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-vmdc-inline-vmcluster",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/managed-by": "vmdistributedcluster",
+						"app.kubernetes.io/instance":   "test-vmdc",
+					},
+					Annotations: map[string]string{
+						"vmdistributedcluster.victoriametrics.com/inline-index": "0",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "operator.victoriametrics.com/v1alpha1",
+							Kind:       "VMDistributedCluster",
+							Name:       "test-vmdc",
+						},
+					},
+				},
+				Spec: vmv1beta1.VMClusterSpec{
+					Image: "victoriametrics/vmcluster:new-version",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Returns error if getting VMCluster fails",
+			args: args{
+				crName:    "test-vmdc",
+				namespace: "default",
+				index:     0,
+				refOrSpec: vmv1alpha1.VMClusterRefOrSpec{
+					Name: "inline-vmcluster",
+					VMClusterSpec: &vmv1beta1.VMClusterSpec{
+						Image: "victoriametrics/vmcluster:latest",
+					},
+				},
+			},
+			existingObjs: []runtime.Object{
+				&vmv1beta1.VMCluster{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-vmdc-inline-vmcluster-error", // Different name to simulate not found, but other error
+						Namespace: "default",
+					},
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -103,7 +190,7 @@ func Test_reconcileInlineVMCluster(t *testing.T) {
 
 			// Since reconcileInlineVMCluster calls setOwnerAndDefault, we need to mock or
 			// ensure setOwnerAndDefault behaves as expected for testing.
-			// For simplicity, let's just make sure the owner reference is set in the expected object.
+			// For simplicity, let\'s just make sure the owner reference is set in the expected object.
 			if tt.want != nil {
 				_ = setOwnerFn(context.Background(), rclient, tt.want, vmDistCluster)
 			}

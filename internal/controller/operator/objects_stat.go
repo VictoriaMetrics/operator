dfe72fcbb0897fb4769962e6048427c87c4b620a
Vadim Rutkovsky <vadim@vrutkovs.eu>
2025-11-12 12:07:02 +0100

    Properly retry vmdistributedcluster metrics

--- internal/controller/operator/objects_stat.go
+++ internal/controller/operator/objects_stat.go
@@ -49,7 +49,7 @@ func newCollector() *objectCollector {
 		"vmagent", "vmalert", "vmsingle", "vmcluster", "vmalertmanager", "vmauth", "vlogs", "vlsingle",
 		"vlcluster", "vmalertmanagerconfig", "vmrule", "vmuser", "vmservicescrape", "vmstaticscrape",
 		"vmnodescrape", "vmpodscrape", "vmprobescrape", "vmscrapeconfig", "vmanomaly", "vlagent",
-		"vtsingle", "vtcluster",
+		"vtsingle", "vtcluster", "vmdistributedcluster",
 	}
 	for _, controller := range registeredObjects {
 		oc.objectsByController[controller] = map[string]struct{}{}

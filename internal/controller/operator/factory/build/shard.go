1a308796ea2c2effca47983d77f8b91870523790
Andrii Chubatiuk <achubatiuk@victoriametrics.com>
2025-11-17 11:09:36 +0200

    vmagent and vmanomaly: create PDB per shard to guarantee proper application protection
    before there was a single PDB, which could lead to unpredicted application disruptions.
    
    Fixes https://github.com/VictoriaMetrics/operator/issues/1548

--- internal/controller/operator/factory/build/shard.go
+++ internal/controller/operator/factory/build/shard.go
@@ -5,6 +5,9 @@ import (
 	"iter"
 	"strconv"
 
+	policyv1 "k8s.io/api/policy/v1"
+
+	vmv1beta1 "github.com/VictoriaMetrics/operator/api/operator/v1beta1"
 	"github.com/VictoriaMetrics/operator/internal/controller/operator/factory/k8stools"
 )
 
@@ -13,11 +16,10 @@ const (
 	shardLabelName      = "shard-num"
 )
 
-type shardedApp interface {
-	PrefixedName() string
-	IsSharded() bool
-	SelectorLabels() map[string]string
+type shardOpts interface {
+	builderOpts
 	PodLabels() map[string]string
+	IsSharded() bool
 }
 
 // ShardNumIter iterates over shardCount in order defined in backward
@@ -43,27 +45,27 @@ func ShardNumIter(backward bool, shardCount int) iter.Seq[int] {
 }
 
 // ShardName adds shard suffix to a name if shards count is > 1
-func ShardName(app shardedApp) string {
-	name := app.PrefixedName()
-	if app.IsSharded() {
+func ShardName(cr shardOpts) string {
+	name := cr.PrefixedName()
+	if cr.IsSharded() {
 		name = fmt.Sprintf("%s-%s", name, shardNumPlaceholder)
 	}
 	return name
 }
 
 // ShardSelectorLabels adds shard label pair if sharding enabled
-func ShardSelectorLabels(app shardedApp) map[string]string {
-	labels := app.SelectorLabels()
-	if app.IsSharded() {
+func ShardSelectorLabels(cr shardOpts) map[string]string {
+	labels := cr.SelectorLabels()
+	if cr.IsSharded() {
 		labels[shardLabelName] = shardNumPlaceholder
 	}
 	return labels
 }
 
 // ShardPodLabels adds shard label pair if sharding enabled
-func ShardPodLabels(app shardedApp) map[string]string {
-	labels := app.PodLabels()
-	if app.IsSharded() {
+func ShardPodLabels(cr shardOpts) map[string]string {
+	labels := cr.PodLabels()
+	if cr.IsSharded() {
 		labels[shardLabelName] = shardNumPlaceholder
 	}
 	return labels
@@ -75,4 +77,14 @@ func RenderShard[T any](resource *T, num int) (*T, error) {
 		shardNumPlaceholder: strconv.Itoa(num),
 	}
 	return k8stools.RenderPlaceholders(resource, placeholders)
+}
+
+// ShardPodDisruptionBudget creates object for given CRD and shard num
+func ShardPodDisruptionBudget(cr shardOpts, spec *vmv1beta1.EmbeddedPodDisruptionBudgetSpec, num int) *policyv1.PodDisruptionBudget {
+	pdb := PodDisruptionBudget(cr, spec)
+	if cr.IsSharded() {
+		pdb.Name = fmt.Sprintf("%s-%d", pdb.Name, num)
+		pdb.Spec.Selector.MatchLabels[shardLabelName] = strconv.Itoa(num)
+	}
+	return pdb
 }
--- internal/controller/operator/factory/finalize/orphaned_test.go
+++ internal/controller/operator/factory/finalize/orphaned_test.go
@@ -15,101 +15,113 @@ import (
 )
 
 func TestRemoveOrphanedDeployments(t *testing.T) {
-	type args struct {
-		ctx             context.Context
-		cr              orphanedCRD
-		keepDeployments map[string]struct{}
-	}
-	tests := []struct {
-		name              string
-		args              args
+	type opts struct {
+		cr                orphanedCRD
+		keepDeployments   map[string]struct{}
 		wantDepCount      int
 		wantErr           bool
 		predefinedObjects []runtime.Object
-	}{
-		{
-			name: "remove nothing",
-			args: args{
-				ctx: context.TODO(),
-				cr: &vmv1beta1.VMAgent{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "base",
-						Namespace: "default",
-					},
-				},
-				keepDeployments: map[string]struct{}{"base": {}},
-			},
-			predefinedObjects: []runtime.Object{
-				&appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "base",
-						Namespace: "default",
-						Labels: map[string]string{
-							"app.kubernetes.io/name":      "vmagent",
-							"app.kubernetes.io/instance":  "base",
-							"app.kubernetes.io/component": "monitoring",
-							"managed-by":                  "vm-operator",
-						},
-					},
-				},
-			},
-			wantDepCount: 1,
-		},
-		{
-			name: "remove 1 orphaned",
-			args: args{
-				ctx: context.TODO(),
-				cr: &vmv1beta1.VMAgent{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "base",
-						Namespace: "default",
-					},
-				},
-				keepDeployments: map[string]struct{}{"base-0": {}},
-			},
-			predefinedObjects: []runtime.Object{
-				&appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "base-0",
-						Namespace: "default",
-						Labels: map[string]string{
-							"app.kubernetes.io/name":      "vmagent",
-							"app.kubernetes.io/instance":  "base",
-							"app.kubernetes.io/component": "monitoring",
-							"managed-by":                  "vm-operator",
-						},
-					},
-				},
-				&appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "base-1",
-						Namespace: "default",
-						Labels: map[string]string{
-							"app.kubernetes.io/name":      "vmagent",
-							"app.kubernetes.io/instance":  "base",
-							"app.kubernetes.io/component": "monitoring",
-							"managed-by":                  "vm-operator",
-						},
-					},
-				},
-			},
-			wantDepCount: 1,
-		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cl := k8stools.GetTestClientWithObjects(tt.predefinedObjects)
-			if err := RemoveOrphanedDeployments(tt.args.ctx, cl, tt.args.cr, tt.args.keepDeployments); (err != nil) != tt.wantErr {
-				t.Errorf("RemoveOrphanedDeployments() error = %v, wantErr %v", err, tt.wantErr)
-			}
-			var existDep appsv1.DeploymentList
-			opts := client.ListOptions{Namespace: tt.args.cr.GetNamespace(), LabelSelector: labels.SelectorFromSet(tt.args.cr.SelectorLabels())}
-			if err := cl.List(tt.args.ctx, &existDep, &opts); err != nil {
-				t.Fatalf("unexpected error: %v", err)
-			}
-			if tt.wantDepCount != len(existDep.Items) {
-				t.Fatalf("unexpected count of deployments, got:%v, want: %v", len(existDep.Items), tt.wantDepCount)
-			}
-		})
+	f := func(o opts) {
+		t.Helper()
+		cl := k8stools.GetTestClientWithObjects(o.predefinedObjects)
+		ctx := context.TODO()
+		if err := RemoveOrphanedDeployments(ctx, cl, o.cr, o.keepDeployments); (err != nil) != o.wantErr {
+			t.Errorf("RemoveOrphanedDeployments() error = %v, wantErr %v", err, o.wantErr)
+		}
+		var existDep appsv1.DeploymentList
+		lo := client.ListOptions{Namespace: o.cr.GetNamespace(), LabelSelector: labels.SelectorFromSet(o.cr.SelectorLabels())}
+		if err := cl.List(ctx, &existDep, &lo); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if o.wantDepCount != len(existDep.Items) {
+			t.Fatalf("unexpected count of deployments, got:%v, want: %v", len(existDep.Items), o.wantDepCount)
+		}
 	}
+
+	// remove nothing
+	f(opts{
+		cr: &vmv1beta1.VMAgent{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "base",
+				Namespace: "default",
+			},
+		},
+		keepDeployments: map[string]struct{}{"base": {}},
+		predefinedObjects: []runtime.Object{
+			&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "base",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "vmagent",
+						"app.kubernetes.io/instance":  "base",
+						"app.kubernetes.io/component": "monitoring",
+						"managed-by":                  "vm-operator",
+					},
+				},
+			},
+		},
+		wantDepCount: 1,
+	})
+
+	// remove 1 orphaned
+	f(opts{
+		cr: &vmv1beta1.VMAgent{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "base",
+				Namespace: "default",
+			},
+		},
+		keepDeployments: map[string]struct{}{"base-0": {}},
+		predefinedObjects: []runtime.Object{
+			&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "base-0",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "vmagent",
+						"app.kubernetes.io/instance":  "base",
+						"app.kubernetes.io/component": "monitoring",
+						"managed-by":                  "vm-operator",
+					},
+				},
+			},
+			&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "base-1",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "vmagent",
+						"app.kubernetes.io/instance":  "base",
+						"app.kubernetes.io/component": "monitoring",
+						"managed-by":                  "vm-operator",
+					},
+					Finalizers: []string{
+						vmv1beta1.FinalizerName,
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "v1beta1",
+							Kind:       "VMAgent",
+							Name:       "base",
+						},
+					},
+				},
+			},
+			&appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "base-2",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "vmagent",
+						"app.kubernetes.io/instance":  "base",
+						"app.kubernetes.io/component": "monitoring",
+						"managed-by":                  "vm-operator",
+					},
+				},
+			},
+		},
+		wantDepCount: 2,
+	})
 }

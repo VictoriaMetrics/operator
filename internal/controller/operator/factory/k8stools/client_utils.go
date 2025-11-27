3d30d8edef30580db9348d64f0af384107862810
sure <bigfemonkey@gmail.com>
2025-11-27 21:52:18 +0800

    fix: remove HTTPRoute check

--- internal/controller/operator/factory/k8stools/client_utils.go
+++ internal/controller/operator/factory/k8stools/client_utils.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	corev1 "k8s.io/api/core/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
@@ -262,8 +261,4 @@ func FetchConfigMapContentByKey(ctx context.Context, rclient client.Client, cm *
 		return "", err
 	}
 	return cm.Data[key], nil
-}
-
-func GetCRD(ctx context.Context, c client.Client, crdName string) error {
-	return c.Get(ctx, client.ObjectKey{Name: crdName}, &apiextensionsv1.CustomResourceDefinition{})
 }

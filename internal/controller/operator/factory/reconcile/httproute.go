8b394e0f52da08ab75ef91351268f74e9a2ad4b5
f41gh7 <nik@victoriametrics.com>
2025-12-02 14:51:23 +0100

    follow-up for 94453d4966b7ac7cc240c599b22994ffd9b65a1c
     This commit adds proper reconcile equality check for httpRoute.
    Previously, it didn't take in account default fields added by
    gateway-controller.

--- internal/controller/operator/factory/reconcile/httproute.go
+++ internal/controller/operator/factory/reconcile/httproute.go
@@ -18,6 +18,8 @@ import (
 func HTTPRoute(ctx context.Context, rclient client.Client, newHTTPRoute, prevHTTPRoute *gwapiv1.HTTPRoute) error {
 	return retryOnConflict(func() error {
 		var curHTTPRoute gwapiv1.HTTPRoute
+		var isPrevEqual bool
+
 		if err := rclient.Get(ctx, types.NamespacedName{Name: newHTTPRoute.GetName(), Namespace: newHTTPRoute.GetNamespace()}, &curHTTPRoute); err != nil {
 			if k8serrors.IsNotFound(err) {
 				logger.WithContext(ctx).Info(fmt.Sprintf("creating HTTPRoute %s configuration", newHTTPRoute.Name))
@@ -34,7 +36,13 @@ func HTTPRoute(ctx context.Context, rclient client.Client, newHTTPRoute, prevHTT
 			return err
 		}
 
-		if equality.Semantic.DeepEqual(newHTTPRoute.Spec, curHTTPRoute.Spec) &&
+		// use DeepDerivative compare since gatewayapi controller sets default fields to the parentRefs fields
+		isEqual := equality.Semantic.DeepDerivative(newHTTPRoute.Spec, curHTTPRoute.Spec)
+		if prevHTTPRoute != nil {
+			isPrevEqual = equality.Semantic.DeepDerivative(prevHTTPRoute.Spec, newHTTPRoute.Spec)
+		}
+		if isEqual &&
+			isPrevEqual &&
 			isObjectMetaEqual(&curHTTPRoute, newHTTPRoute, prevHTTPRoute) {
 			return nil
 		}
@@ -42,7 +50,10 @@ func HTTPRoute(ctx context.Context, rclient client.Client, newHTTPRoute, prevHTT
 		mergeObjectMetadataIntoNew(&curHTTPRoute, newHTTPRoute, prevHTTPRoute)
 		newHTTPRoute.Status = curHTTPRoute.Status
 
-		logMsg := fmt.Sprintf("updating HTTPRoute %s configuration spec_diff: %s", newHTTPRoute.Name, diffDeepDerivative(newHTTPRoute.Spec, curHTTPRoute.Spec))
+		logMsg := fmt.Sprintf("updating HTTPRoute %s configuration spec_diff: %s"+
+			"is_prev_equal=%v,is_current_equal=%v,is_prev_nil=%v",
+			newHTTPRoute.Name, diffDeepDerivative(newHTTPRoute.Spec, curHTTPRoute.Spec), isPrevEqual, isEqual, prevHTTPRoute == nil)
+
 		logger.WithContext(ctx).Info(logMsg)
 
 		return rclient.Update(ctx, newHTTPRoute)

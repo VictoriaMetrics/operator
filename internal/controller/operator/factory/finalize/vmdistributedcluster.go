b60f1b407e77232cea7d3550c7a128a32cbf6914
Vadim Rutkovsky <vadim@vrutkovs.eu>
2025-12-02 10:59:36 +0100

    Delete vmauthlb artifacts when vmdistributedcluster is being delete

--- internal/controller/operator/factory/finalize/vmdistributedcluster.go
+++ internal/controller/operator/factory/finalize/vmdistributedcluster.go
@@ -3,6 +3,9 @@ package finalize
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	policyv1 "k8s.io/api/policy/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	vmv1alpha1 "github.com/VictoriaMetrics/operator/api/operator/v1alpha1"
@@ -19,18 +22,51 @@ func OnVMDistributedClusterDelete(ctx context.Context, rclient client.Client, ob
 		}
 	}
 
-	// Remove created VMAuth
-	vmauth := &vmv1beta1.VMAuth{}
-	if err := rclient.Get(ctx, client.ObjectKey{Name: obj.Spec.VMAuth.Name, Namespace: obj.Namespace}, vmauth); err == nil {
-		if err := rclient.Delete(ctx, vmauth); err != nil {
+	lbPrefixedName := obj.PrefixedName(vmv1beta1.ClusterComponentBalancer)
+	// Remove vmauthlb deployment
+	vmauthlb := &appsv1.Deployment{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: lbPrefixedName, Namespace: obj.Namespace}, vmauthlb); err == nil {
+		if err := rclient.Delete(ctx, vmauthlb); err != nil {
 			return err
 		}
 	}
 
-	// Remove created VMUser
-	vmuser := &vmv1beta1.VMUser{}
-	if err := rclient.Get(ctx, client.ObjectKey{Name: obj.GetVMUserName(), Namespace: obj.Namespace}, vmuser); err == nil {
-		if err := rclient.Delete(ctx, vmuser); err != nil {
+	// Remove vmauthlb service
+	vmauthlbService := &corev1.Service{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: obj.Spec.VMAuth.Name, Namespace: obj.Namespace}, vmauthlbService); err == nil {
+		if err := rclient.Delete(ctx, vmauthlbService); err != nil {
+			return err
+		}
+	}
+
+	// Remove vmauthlb serviceaccount
+	vmauthlbServiceAccount := &corev1.ServiceAccount{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: obj.GetServiceAccountName(), Namespace: obj.Namespace}, vmauthlbServiceAccount); err == nil {
+		if err := rclient.Delete(ctx, vmauthlbServiceAccount); err != nil {
+			return err
+		}
+	}
+
+	// Remove vmauthlb secret
+	vmauthlbSecret := &corev1.Secret{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: lbPrefixedName, Namespace: obj.Namespace}, vmauthlbSecret); err == nil {
+		if err := rclient.Delete(ctx, vmauthlbSecret); err != nil {
+			return err
+		}
+	}
+
+	// Remove vmauthlb servicescraper
+	vmauthlbServiceScraper := &vmv1beta1.VMServiceScrape{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: obj.Spec.VMAuth.Name, Namespace: obj.Namespace}, vmauthlbServiceScraper); err == nil {
+		if err := rclient.Delete(ctx, vmauthlbServiceScraper); err != nil {
+			return err
+		}
+	}
+
+	// Remove vmauthlb PDB
+	vmauthlbPDB := &policyv1.PodDisruptionBudget{}
+	if err := rclient.Get(ctx, client.ObjectKey{Name: lbPrefixedName, Namespace: obj.Namespace}, vmauthlbPDB); err == nil {
+		if err := rclient.Delete(ctx, vmauthlbPDB); err != nil {
 			return err
 		}
 	}

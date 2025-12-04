6dd51d400b9c0dc870b5a60ac781a57fd02928bf
Vadim Rutkovsky <vadim@vrutkovs.eu>
2025-12-04 10:01:37 +0100

    Fix addVMDistributedClusterDefaults comparison

--- internal/controller/operator/factory/build/defaults.go
+++ internal/controller/operator/factory/build/defaults.go
@@ -1006,7 +1006,7 @@ func addVMDistributedClusterDefaults(objI any) {
 	c := getCfg()
 
 	cv := config.ApplicationDefaults(c.VMAuthDefault)
-	if cr.Spec.VMAuth.Spec != nil {
+	if cr.Spec.VMAuth.Spec == nil {
 		cr.Spec.VMAuth.Spec = &vmv1beta1.VMAuthLoadBalancerSpec{}
 	}
 	addDefaultsToCommonParams(&cr.Spec.VMAuth.Spec.CommonDefaultableParams, nil, &cv)

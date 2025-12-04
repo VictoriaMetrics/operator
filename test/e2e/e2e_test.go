1802403d92d4dc167085266c8565b91268c75a12
Vadim Rutkovsky <vadim@vrutkovs.eu>
2025-12-04 11:49:25 +0100

    Add VMAgentFlushDeadline

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -34,7 +34,7 @@ var (
 	eventualDeletionTimeout                      = 45 * time.Second
 	eventualDeploymentPodTimeout                 = 25 * time.Second
 	eventualExpandingTimeout                     = 25 * time.Second
-	eventualVMDistributedClusterExpandingTimeout = 2 * time.Minute
+	eventualVMDistributedClusterExpandingTimeout = 5 * time.Minute
 )
 
 // Run e2e tests using the Ginkgo runner.

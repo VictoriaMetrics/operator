090019fc5e1e072bfff95e731ad52f23b70ab424
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-01-09 14:02:41 +0100

    finalize: make sure correct service is being removed

--- internal/controller/operator/factory/finalize/vmdistributedcluster.go
+++ internal/controller/operator/factory/finalize/vmdistributedcluster.go
@@ -33,6 +33,10 @@ func OnVMDistributedClusterDelete(ctx context.Context, rclient client.Client, cr
 		}
 		objsToRemove = append(objsToRemove, &appsv1.Deployment{ObjectMeta: vmAuthLBPrefixedMeta})
 		objsToRemove = append(objsToRemove, &corev1.Service{ObjectMeta: vmAuthLBPrefixedMeta})
+		objsToRemove = append(objsToRemove, &corev1.Service{ObjectMeta: metav1.ObjectMeta{
+			Namespace: ns,
+			Name:      cr.Spec.VMAuth.Name,
+		}})
 		objsToRemove = append(objsToRemove, &corev1.ServiceAccount{ObjectMeta: vmAuthLBPrefixedMeta})
 		objsToRemove = append(objsToRemove, &corev1.Secret{ObjectMeta: vmAuthLBPrefixedMeta})
 		objsToRemove = append(objsToRemove, &rbacv1.Role{ObjectMeta: vmAuthLBPrefixedMeta})

578c068c72e91b5243577cda56ea92478b0d5628
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-01-13 10:27:20 +0100

    VMD: create VMAuth object instead of separate resources

--- internal/controller/operator/factory/vmdistributedcluster/vmauth_config_test.go
+++ internal/controller/operator/factory/vmdistributedcluster/vmauth_config_test.go
@@ -1,189 +1,203 @@
 package vmdistributedcluster
 
 import (
-	"bytes"
-	"compress/gzip"
-	"io"
+	"context"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 
-	vmv1alpha1 "github.com/VictoriaMetrics/operator/api/operator/v1alpha1"
 	vmv1beta1 "github.com/VictoriaMetrics/operator/api/operator/v1beta1"
 )
 
-func Test_buildVMAuthVMSelectURLMaps(t *testing.T) {
-	t.Run("single default vmcluster", func(t *testing.T) {
-		vmClusters := []*vmv1beta1.VMCluster{
-			{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "vmc-test",
-					Namespace: "default",
-				},
-				Spec: vmv1beta1.VMClusterSpec{},
-			},
-		}
-		want := URLMap{
-			SrcPaths:           []string{"/.*"},
-			URLPrefix:          []string{"http://srv+vmselect-vmc-test.default.svc:8481"},
-			DiscoverBackendIPs: true,
-		}
-		got := buildVMAuthVMSelectURLMaps(vmClusters)
-		assert.Equal(t, want, got)
+func TestCreateOrUpdateVMAuthLB(t *testing.T) {
+	t.Run("should do nothing if VMAuth name is empty", func(t *testing.T) {
+		data := beforeEach()
+		data.cr.Spec.VMAuth.Name = ""
+		rclient := data.trackingClient
+		ctx := context.Background()
+
+		err := createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, []*vmv1beta1.VMCluster{data.vmcluster1, data.vmcluster2})
+		assert.NoError(t, err)
+		assert.Empty(t, rclient.Actions)
 	})
 
-	t.Run("multiple vmclusters", func(t *testing.T) {
-		vmClusters := []*vmv1beta1.VMCluster{
-			{
-				ObjectMeta: metav1.ObjectMeta{Name: "vmc-1", Namespace: "ns1"},
-				Spec:       vmv1beta1.VMClusterSpec{},
-			},
-			{
-				ObjectMeta: metav1.ObjectMeta{Name: "vmc-2", Namespace: "ns2"},
-				Spec: vmv1beta1.VMClusterSpec{
-					VMSelect: &vmv1beta1.VMSelect{},
-				},
-			},
+	t.Run("should create VMAuth if it does not exist", func(t *testing.T) {
+		data := beforeEach()
+		data.cr.Spec.VMAuth.Name = "vmauth-lb"
+		data.cr.Spec.VMAuth.Spec = &vmv1beta1.VMAuthLoadBalancerSpec{
+			LogLevel: "INFO",
 		}
-		want := URLMap{
-			SrcPaths: []string{"/.*"},
-			URLPrefix: []string{
-				"http://srv+vmselect-vmc-1.ns1.svc:8481",
-				"http://srv+vmselect-vmc-2.ns2.svc:8481",
-			},
-			DiscoverBackendIPs: true,
+		rclient := data.trackingClient
+		ctx := context.Background()
+
+		clusters := []*vmv1beta1.VMCluster{data.vmcluster1, data.vmcluster2}
+		err := createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, clusters)
+		assert.NoError(t, err)
+
+		// Check actions
+		var createAction *action
+		for _, a := range rclient.Actions {
+			if a.Method == "Create" {
+				if _, ok := a.Object.(*vmv1beta1.VMAuth); ok {
+					createAction = &a
+					break
+				}
+			}
 		}
-		got := buildVMAuthVMSelectURLMaps(vmClusters)
-		assert.Equal(t, want, got)
+		require.NotNil(t, createAction, "VMAuth should be created")
+
+		createdVMAuth := createAction.Object.(*vmv1beta1.VMAuth)
+		assert.Equal(t, "vmauth-lb", createdVMAuth.Name)
+		assert.Equal(t, "default", createdVMAuth.Namespace)
+		assert.Equal(t, "INFO", createdVMAuth.Spec.LogLevel)
+
+		require.NotNil(t, createdVMAuth.Spec.UnauthorizedUserAccessSpec)
+		require.Len(t, createdVMAuth.Spec.UnauthorizedUserAccessSpec.URLMap, 1)
+		urlMap := createdVMAuth.Spec.UnauthorizedUserAccessSpec.URLMap[0]
+
+		expectedURLs := []string{
+			"http://vmselect-vmcluster-1.default.svc:8481/",
+			"http://vmselect-vmcluster-2.default.svc:8481/",
+		}
+		sort.Strings(expectedURLs)
+		assert.Equal(t, vmv1beta1.StringOrArray(expectedURLs), urlMap.URLPrefix)
+		assert.Equal(t, []string{"/select/.+", "/admin/tenants"}, urlMap.SrcPaths)
+		assert.Equal(t, ptr.To(0), urlMap.DropSrcPathPrefixParts)
+		assert.Equal(t, ptr.To("first_available"), urlMap.LoadBalancingPolicy)
+		assert.Equal(t, []int{500, 502, 503}, urlMap.RetryStatusCodes)
+
+		// Verify OwnerReference
+		assert.NotEmpty(t, createdVMAuth.OwnerReferences)
+		assert.Equal(t, data.cr.Name, createdVMAuth.OwnerReferences[0].Name)
 	})
 
-	t.Run("no vmclusters", func(t *testing.T) {
-		vmClusters := []*vmv1beta1.VMCluster{}
-		want := URLMap{
-			SrcPaths:           []string{"/.*"},
-			URLPrefix:          []string{},
-			DiscoverBackendIPs: true,
-		}
-		got := buildVMAuthVMSelectURLMaps(vmClusters)
-		assert.Equal(t, want, got)
-	})
-}
+	t.Run("should update VMAuth if spec changes", func(t *testing.T) {
+		data := beforeEach()
+		data.cr.Spec.VMAuth.Name = "vmauth-lb"
 
-func Test_buildVMAuthLBSecret(t *testing.T) {
-	cr := &vmv1alpha1.VMDistributedCluster{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "vmdist",
-			Namespace: "prod",
-		},
-		Spec: vmv1alpha1.VMDistributedClusterSpec{
-			VMAuth: vmv1alpha1.VMAuthNameAndSpec{
-				Name: "vmauth",
-				Spec: &vmv1beta1.VMAuthLoadBalancerSpec{},
-			},
-		},
-	}
-	vmClusters := []*vmv1beta1.VMCluster{
-		{
+		// Create existing VMAuth with different spec
+		existingVMAuth := &vmv1beta1.VMAuth{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "vmc-test",
+				Name:      "vmauth-lb",
 				Namespace: "default",
 			},
-			Spec: vmv1beta1.VMClusterSpec{},
-		},
-	}
-
-	secret, err := buildVMAuthLBSecret(cr, vmClusters)
-	assert.NoError(t, err)
-	assert.NotNil(t, secret)
-
-	assert.Equal(t, "vmdclusterlb-vmdist", secret.Name)
-	assert.Equal(t, "prod", secret.Namespace)
-
-	configGZData, ok := secret.Data[configGZName]
-	assert.True(t, ok)
-	assert.NotEmpty(t, configGZData)
-
-	gr, err := gzip.NewReader(bytes.NewReader(configGZData))
-	assert.NoError(t, err)
-	defer gr.Close()
-	decompressed, err := io.ReadAll(gr)
-	assert.NoError(t, err)
-
-	expectedYAML := `unauthorized_user:
-  url_map:
-  - src_paths:
-    - /.*
-    url_prefix:
-    - http://srv+vmselect-vmc-test.default.svc:8481
-    discover_backend_ips: true
-`
-	assert.Equal(t, []byte(expectedYAML), decompressed)
-
-	t.Run("multiple vmclusters", func(t *testing.T) {
-		vmClusters := []*vmv1beta1.VMCluster{
-			{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "vmc-test-1",
-					Namespace: "default",
-				},
-				Spec: vmv1beta1.VMClusterSpec{},
-			},
-			{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "vmc-test-2",
-					Namespace: "monitoring",
-				},
-				Spec: vmv1beta1.VMClusterSpec{},
+			Spec: vmv1beta1.VMAuthSpec{
+				LogLevel: "ERROR",
 			},
 		}
-		secret, err := buildVMAuthLBSecret(cr, vmClusters)
-		assert.NoError(t, err)
-		assert.NotNil(t, secret)
 
-		configGZData, ok := secret.Data[configGZName]
-		assert.True(t, ok)
+		rclient := data.trackingClient
+		// Need to create it in the client
+		err := rclient.Create(context.Background(), existingVMAuth)
+		require.NoError(t, err)
+		rclient.Actions = []action{} // Clear create action
 
-		gr, err := gzip.NewReader(bytes.NewReader(configGZData))
-		assert.NoError(t, err)
-		defer gr.Close()
-		decompressed, err := io.ReadAll(gr)
+		data.cr.Spec.VMAuth.Spec = &vmv1beta1.VMAuthLoadBalancerSpec{
+			LogLevel: "INFO",
+		}
+
+		ctx := context.Background()
+		clusters := []*vmv1beta1.VMCluster{data.vmcluster1}
+		err = createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, clusters)
 		assert.NoError(t, err)
 
-		expectedYAML := `unauthorized_user:
-  url_map:
-  - src_paths:
-    - /.*
-    url_prefix:
-    - http://srv+vmselect-vmc-test-1.default.svc:8481
-    - http://srv+vmselect-vmc-test-2.monitoring.svc:8481
-    discover_backend_ips: true
-`
-		assert.Equal(t, []byte(expectedYAML), decompressed)
+		// Check for update action
+		var updateAction *action
+		for _, a := range rclient.Actions {
+			if a.Method == "Update" {
+				if _, ok := a.Object.(*vmv1beta1.VMAuth); ok {
+					updateAction = &a
+					break
+				}
+			}
+		}
+		require.NotNil(t, updateAction, "VMAuth should be updated")
+
+		updatedVMAuth := updateAction.Object.(*vmv1beta1.VMAuth)
+		assert.Equal(t, "INFO", updatedVMAuth.Spec.LogLevel)
 	})
 
-	// Test with no vmclusters
-	t.Run("no vmclusters", func(t *testing.T) {
-		secret, err := buildVMAuthLBSecret(cr, []*vmv1beta1.VMCluster{})
-		assert.NoError(t, err)
-		assert.NotNil(t, secret)
+	t.Run("should not update VMAuth if spec matches", func(t *testing.T) {
+		data := beforeEach()
+		data.cr.Spec.VMAuth.Name = "vmauth-lb"
+		data.cr.Spec.VMAuth.Spec = &vmv1beta1.VMAuthLoadBalancerSpec{
+			LogLevel: "INFO",
+		}
 
-		configGZData, ok := secret.Data[configGZName]
-		assert.True(t, ok)
+		// Construct the expected VMAuth to exist already
+		ctx := context.Background()
+		clusters := []*vmv1beta1.VMCluster{data.vmcluster1}
 
-		gr, err := gzip.NewReader(bytes.NewReader(configGZData))
-		assert.NoError(t, err)
-		defer gr.Close()
-		decompressed, err := io.ReadAll(gr)
+		// We first let the function create it to get the exact state
+		rclient := data.trackingClient
+		err := createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, clusters)
+		require.NoError(t, err)
+
+		rclient.Actions = []action{} // Clear actions
+
+		// Run again
+		err = createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, clusters)
 		assert.NoError(t, err)
 
-		expectedYAML := `unauthorized_user:
-  url_map:
-  - src_paths:
-    - /.*
-    url_prefix: []
-    discover_backend_ips: true
-`
-		assert.Equal(t, []byte(expectedYAML), decompressed)
+		// Should contain no updates
+		for _, a := range rclient.Actions {
+			if a.Method == "Update" {
+				if _, ok := a.Object.(*vmv1beta1.VMAuth); ok {
+					t.Fatalf("Unexpected update action on VMAuth")
+				}
+			}
+		}
+	})
+
+	t.Run("should adopt existing VMAuth if owner reference is missing", func(t *testing.T) {
+		data := beforeEach()
+		data.cr.Spec.VMAuth.Name = "vmauth-lb"
+		data.cr.Spec.VMAuth.Spec = &vmv1beta1.VMAuthLoadBalancerSpec{
+			LogLevel: "INFO",
+		}
+
+		rclient := data.trackingClient
+
+		// Create VMAuth without owner ref
+		existingVMAuth := &vmv1beta1.VMAuth{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "vmauth-lb",
+				Namespace: "default",
+			},
+			Spec: vmv1beta1.VMAuthSpec{
+				// We intentionally don't set a full spec here,
+				// so the update will populate the missing fields (like Port)
+				// AND set the owner ref.
+				LogLevel: "INFO",
+			},
+		}
+		err := rclient.Create(context.Background(), existingVMAuth)
+		require.NoError(t, err)
+		rclient.Actions = []action{}
+
+		ctx := context.Background()
+		clusters := []*vmv1beta1.VMCluster{data.vmcluster1}
+		err = createOrUpdateVMAuthLB(ctx, rclient, data.cr, nil, clusters)
+		assert.NoError(t, err)
+
+		// Expect Update action
+		var updateAction *action
+		for _, a := range rclient.Actions {
+			if a.Method == "Update" {
+				if _, ok := a.Object.(*vmv1beta1.VMAuth); ok {
+					updateAction = &a
+					break
+				}
+			}
+		}
+		require.NotNil(t, updateAction, "VMAuth should be updated with owner ref")
+
+		updatedVMAuth := updateAction.Object.(*vmv1beta1.VMAuth)
+		assert.NotEmpty(t, updatedVMAuth.OwnerReferences)
+		assert.Equal(t, data.cr.Name, updatedVMAuth.OwnerReferences[0].Name)
 	})
 }

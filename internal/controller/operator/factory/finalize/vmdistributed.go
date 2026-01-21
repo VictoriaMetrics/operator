a3b07ed49f1523d723ccb2bbdcff23897df337c6
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2026-01-21 22:44:50 +0200

    before this commit operator removed finalizer for child VM objects which lead to dangling resources since finalizer for parent resource should be removed once finalizers for children are removed

--- internal/controller/operator/factory/finalize/vmdistributed.go
+++ internal/controller/operator/factory/finalize/vmdistributed.go
@@ -17,25 +17,7 @@ import (
 
 // OnVMDistributedDelete removes all objects related to VMDistributed component
 func OnVMDistributedDelete(ctx context.Context, rclient client.Client, cr *vmv1alpha1.VMDistributed) error {
-	ns := cr.GetNamespace()
-	objsToRemove := []client.Object{}
 	objsToDisown := []client.Object{}
-	if len(cr.Spec.VMAgent.Name) > 0 {
-		objsToRemove = append(objsToRemove, &vmv1beta1.VMAgent{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: ns,
-				Name:      cr.Spec.VMAgent.Name,
-			},
-		})
-	}
-	if len(cr.Spec.VMAuth.Name) > 0 {
-		objsToRemove = append(objsToRemove, &vmv1beta1.VMAuth{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: ns,
-				Name:      cr.Spec.VMAuth.Name,
-			},
-		})
-	}
 	for _, vmclusterSpec := range cr.Spec.Zones.VMClusters {
 		if vmclusterSpec.Ref != nil && len(vmclusterSpec.Ref.Name) > 0 {
 			objsToDisown = append(objsToDisown, &vmv1beta1.VMCluster{
@@ -44,37 +26,6 @@ func OnVMDistributedDelete(ctx context.Context, rclient client.Client, cr *vmv1a
 					Namespace: cr.Namespace,
 				},
 			})
-		} else if len(vmclusterSpec.Name) > 0 {
-			objsToRemove = append(objsToRemove, &vmv1beta1.VMCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      vmclusterSpec.Name,
-					Namespace: cr.Namespace,
-				},
-			})
-		}
-	}
-	for _, objToRemove := range objsToRemove {
-		nsn := types.NamespacedName{
-			Name:      objToRemove.GetName(),
-			Namespace: objToRemove.GetNamespace(),
-		}
-		err := wait.PollUntilContextTimeout(ctx, time.Second, 5*time.Second, true, func(ctx context.Context) (done bool, err error) {
-			if err := rclient.Get(ctx, nsn, objToRemove); err != nil {
-				if k8serrors.IsNotFound(err) {
-					return true, nil
-				}
-				return false, fmt.Errorf("error looking for object: %w", err)
-			}
-			if err := removeFinalizeObjByNameWithOwnerReference(ctx, rclient, objToRemove, cr.Name, cr.Namespace, false); err != nil {
-				return false, err
-			}
-			if err := SafeDelete(ctx, rclient, objToRemove); err != nil {
-				return false, err
-			}
-			return true, nil
-		})
-		if err != nil {
-			return fmt.Errorf("failed to remove object %T=%s: %w", objToRemove, nsn.String(), err)
 		}
 	}
 	owner := cr.AsOwner()

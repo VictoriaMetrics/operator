f4361633858061e850fc1f4aeb3b83d29e4a3b56
Max Kotliar <mkotlyar@victoriametrics.com>
2026-02-02 20:10:59 +0200

    internal/config: update VM_METRICS_VERSION to release v1.135.0
    Changelog: https://github.com/VictoriaMetrics/VictoriaMetrics/blob/master/docs/victoriametrics/changelog/CHANGELOG.md#v11350

--- internal/config/config.go
+++ internal/config/config.go
@@ -34,7 +34,7 @@ var (
 	initConf sync.Once
 
 	defaultEnvs = map[string]string{
-		"VM_METRICS_VERSION":  "v1.134.0",
+		"VM_METRICS_VERSION":  "v1.135.0",
 		"VM_LOGS_VERSION":     "v1.44.0",
 		"VM_ANOMALY_VERSION":  "v1.28.5",
 		"VM_TRACES_VERSION":   "v0.7.0",

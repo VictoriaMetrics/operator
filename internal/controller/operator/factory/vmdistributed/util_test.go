3f9213c2d13708e7e33daf021673b0341f5c19c4
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-02-20 12:42:00 +0100

    test: add unit tests for %ZONE% templating (#1835)
    * test: add unit tests for %ZONE% templating
    
    Ensure that %ZONE% can be templated anywhere in VMCluster / VMAgent spec
    
    * Update internal/controller/operator/factory/vmdistributed/util_test.go
    
    Co-authored-by: Andrii Chubatiuk <achubatiuk@victoriametrics.com>
    Signed-off-by: Vadim Rutkovsky <roignac@gmail.com>
    
    ---------
    
    Signed-off-by: Vadim Rutkovsky <roignac@gmail.com>
    Co-authored-by: Andrii Chubatiuk <achubatiuk@victoriametrics.com>

--- internal/controller/operator/factory/vmdistributed/util_test.go
+++ internal/controller/operator/factory/vmdistributed/util_test.go
@@ -9,6 +9,11 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/utils/ptr"
+
+	vmv1alpha1 "github.com/VictoriaMetrics/operator/api/operator/v1alpha1"
+	vmv1beta1 "github.com/VictoriaMetrics/operator/api/operator/v1beta1"
 )
 
 func TestFetchMetricValues(t *testing.T) {
@@ -33,5 +38,83 @@ func TestFetchMetricValues(t *testing.T) {
 test_metric{path="no path"} 24
 `, "test_metric", "path", map[string]float64{
 		"no path": 24,
+	})
+}
+
+func TestMergeSpecs(t *testing.T) {
+	t.Run("VMCluster", func(t *testing.T) {
+		f := func(common, zone *vmv1beta1.VMClusterSpec, zoneName string, expected *vmv1beta1.VMClusterSpec) {
+			t.Helper()
+			got, err := mergeSpecs(common, zone, zoneName)
+			require.NoError(t, err)
+			assert.Equal(t, expected, got)
+		}
+
+		// zone-specific retention period
+		f(&vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "1d",
+			VMStorage: &vmv1beta1.VMStorage{
+				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+					ReplicaCount: ptr.To(int32(1)),
+				},
+			},
+		}, &vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "30d",
+		}, "zone-a", &vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "30d",
+			VMStorage: &vmv1beta1.VMStorage{
+				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+					ReplicaCount: ptr.To(int32(1)),
+				},
+			},
+		})
+
+		// %ZONE% templating
+		f(&vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "1d",
+			VMStorage: &vmv1beta1.VMStorage{
+				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+					ReplicaCount: ptr.To(int32(1)),
+					NodeSelector: map[string]string{
+						"topology.kubernetes.io/zone": "%ZONE%",
+					},
+				},
+			},
+		}, &vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "30d",
+		}, "zone-a", &vmv1beta1.VMClusterSpec{
+			RetentionPeriod: "30d",
+			VMStorage: &vmv1beta1.VMStorage{
+				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+					ReplicaCount: ptr.To(int32(1)),
+					NodeSelector: map[string]string{
+						"topology.kubernetes.io/zone": "zone-a",
+					},
+				},
+			},
+		})
+	})
+
+	t.Run("VMAgent", func(t *testing.T) {
+		f := func(common, zone *vmv1alpha1.VMDistributedZoneAgentSpec, zoneName string, expected *vmv1alpha1.VMDistributedZoneAgentSpec) {
+			t.Helper()
+			got, err := mergeSpecs(common, zone, zoneName)
+			require.NoError(t, err)
+			assert.Equal(t, expected, got)
+		}
+
+		f(&vmv1alpha1.VMDistributedZoneAgentSpec{
+			CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+				NodeSelector: map[string]string{
+					"topology.kubernetes.io/zone": "%ZONE%",
+				},
+			},
+		}, &vmv1alpha1.VMDistributedZoneAgentSpec{}, "zone-b", &vmv1alpha1.VMDistributedZoneAgentSpec{
+			CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+				NodeSelector: map[string]string{
+					"topology.kubernetes.io/zone": "zone-b",
+				},
+			},
+		})
 	})
 }

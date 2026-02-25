0b8b17f2c837c2c77e6dd35aec98ff655d1b9115
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-02-25 10:49:48 +0100

    tests: update clientWithActions to record changes for all objects

--- internal/controller/operator/factory/reconcile/utils_test.go
+++ internal/controller/operator/factory/reconcile/utils_test.go
@@ -4,36 +4,37 @@ import (
 	"context"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/interceptor"
 
 	"github.com/VictoriaMetrics/operator/internal/controller/operator/factory/k8stools"
 )
 
-type clientWithActions struct {
-	client.Client
-	actions []string
+type clientAction struct {
+	Verb     string
+	Resource types.NamespacedName
 }
 
-func getTestClient(o client.Object, predefinedObjects []runtime.Object) *clientWithActions {
+type clientWithActions struct {
+	client.Client
+	actions []clientAction
+}
+
+func getTestClient(predefinedObjects []runtime.Object) *clientWithActions {
 	var cwa clientWithActions
+
 	cwa.Client = k8stools.GetTestClientWithObjectsAndInterceptors(predefinedObjects, interceptor.Funcs{
 		Create: func(ctx context.Context, cl client.WithWatch, obj client.Object, opts ...client.CreateOption) error {
-			if o.GetName() == obj.GetName() && o.GetNamespace() == obj.GetNamespace() {
-				cwa.actions = append(cwa.actions, "Create")
-			}
+			cwa.actions = append(cwa.actions, clientAction{Verb: "Create", Resource: client.ObjectKeyFromObject(obj)})
 			return cl.Create(ctx, obj, opts...)
 		},
 		Get: func(ctx context.Context, cl client.WithWatch, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
-			if o.GetName() == key.Name && o.GetNamespace() == key.Namespace {
-				cwa.actions = append(cwa.actions, "Get")
-			}
+			cwa.actions = append(cwa.actions, clientAction{Verb: "Get", Resource: key})
 			return cl.Get(ctx, key, obj, opts...)
 		},
 		Update: func(ctx context.Context, cl client.WithWatch, obj client.Object, opts ...client.UpdateOption) error {
-			if o.GetName() == obj.GetName() && o.GetNamespace() == obj.GetNamespace() {
-				cwa.actions = append(cwa.actions, "Update")
-			}
+			cwa.actions = append(cwa.actions, clientAction{Verb: "Update", Resource: client.ObjectKeyFromObject(obj)})
 			return cl.Update(ctx, obj, opts...)
 		},
 	})

1f5a4f2246bafe81e8533bf40e0fd3d6d6786b70
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-03-03 18:29:23 +0100

    test: add reconciliation tests for vmalertmanager

--- internal/controller/operator/factory/vmalertmanager/vmalertmanager_reconcile_test.go
+++ internal/controller/operator/factory/vmalertmanager/vmalertmanager_reconcile_test.go
@@ -7,14 +7,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/utils/ptr"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	vmv1beta1 "github.com/VictoriaMetrics/operator/api/operator/v1beta1"
 	"github.com/VictoriaMetrics/operator/internal/controller/operator/factory/build"
@@ -23,8 +18,8 @@ import (
 
 func Test_CreateOrUpdate_Actions(t *testing.T) {
 	type args struct {
-		cr                *vmv1beta1.VMAlertmanager
-		predefinedObjects []runtime.Object
+		cr     *vmv1beta1.VMAlertmanager
+		preRun func(c *k8stools.ClientWithActions, cr *vmv1beta1.VMAlertmanager)
 	}
 	type want struct {
 		actions []k8stools.ClientAction
@@ -34,25 +29,15 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 	f := func(args args, want want) {
 		t.Helper()
 
-		// Use local scheme to avoid global scheme pollution
-		s := runtime.NewScheme()
-		_ = scheme.AddToScheme(s)
-		_ = vmv1beta1.AddToScheme(s)
-		build.AddDefaults(s)
-		s.Default(args.cr)
-
-		var actions []k8stools.ClientAction
-		objInterceptors := k8stools.GetInterceptorsWithObjects()
-		actionInterceptor := k8stools.NewActionRecordingInterceptor(&actions, &objInterceptors)
-
-		fclient := fake.NewClientBuilder().
-			WithScheme(s).
-			WithStatusSubresource(&vmv1beta1.VMAlertmanager{}).
-			WithRuntimeObjects(args.predefinedObjects...).
-			WithInterceptorFuncs(actionInterceptor).
-			Build()
-
+		fclient := k8stools.GetTestClientWithActionsAndObjects(nil)
 		ctx := context.TODO()
+		build.AddDefaults(fclient.Scheme())
+		fclient.Scheme().Default(args.cr)
+
+		if args.preRun != nil {
+			args.preRun(fclient, args.cr)
+		}
+
 		err := CreateOrUpdateAlertManager(ctx, args.cr, fclient)
 		if want.err != nil {
 			assert.Error(t, err)
@@ -60,19 +45,19 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 			assert.NoError(t, err)
 		}
 
-		if !assert.Equal(t, len(want.actions), len(actions)) {
-			for i, action := range actions {
+		if !assert.Equal(t, len(want.actions), len(fclient.Actions)) {
+			for i, action := range fclient.Actions {
 				t.Logf("Action %d: %s %s %s", i, action.Verb, action.Kind, action.Resource)
 			}
 		}
 
 		for i, action := range want.actions {
-			if i >= len(actions) {
+			if i >= len(fclient.Actions) {
 				break
 			}
-			assert.Equal(t, action.Verb, actions[i].Verb, "idx %d verb", i)
-			assert.Equal(t, action.Kind, actions[i].Kind, "idx %d kind", i)
-			assert.Equal(t, action.Resource, actions[i].Resource, "idx %d resource", i)
+			assert.Equal(t, action.Verb, fclient.Actions[i].Verb, "idx %d verb", i)
+			assert.Equal(t, action.Kind, fclient.Actions[i].Kind, "idx %d kind", i)
+			assert.Equal(t, action.Resource, fclient.Actions[i].Resource, "idx %d resource", i)
 		}
 	}
 
@@ -80,7 +65,6 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 	namespace := "default"
 	vmalertmanagerName := types.NamespacedName{Namespace: namespace, Name: "vmalertmanager-" + name}
 	objectMeta := metav1.ObjectMeta{Name: name, Namespace: namespace}
-	childObjectMeta := metav1.ObjectMeta{Name: vmalertmanagerName.Name, Namespace: namespace}
 
 	// create vmalertmanager with default config
 	f(args{
@@ -107,7 +91,7 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 			},
 		})
 
-	// update vmalertmanager
+	// update vmalertmanager with no changes
 	f(args{
 		cr: &vmv1beta1.VMAlertmanager{
 			ObjectMeta: objectMeta,
@@ -115,96 +99,18 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
 					ReplicaCount: ptr.To(int32(1)),
 				},
-				RollingUpdateStrategy: appsv1.RollingUpdateStatefulSetStrategyType,
 			},
 		},
-		predefinedObjects: []runtime.Object{
-			&corev1.ServiceAccount{ObjectMeta: childObjectMeta},
-			&rbacv1.Role{ObjectMeta: childObjectMeta},
-			&rbacv1.RoleBinding{ObjectMeta: childObjectMeta},
-			&corev1.Service{
-				ObjectMeta: childObjectMeta,
-				Spec: corev1.ServiceSpec{
-					ClusterIP:                "None",
-					PublishNotReadyAddresses: true,
-					Selector: map[string]string{
-						"app.kubernetes.io/name":      "vmalertmanager",
-						"app.kubernetes.io/instance":  name,
-						"app.kubernetes.io/component": "monitoring",
-						"managed-by":                  "vm-operator",
-					},
-					Ports: []corev1.ServicePort{
-						{
-							Name:       "web",
-							Protocol:   "TCP",
-							Port:       9093,
-							TargetPort: intstr.FromInt(9093),
-						},
-						{
-							Name:       "tcp-mesh",
-							Port:       9094,
-							TargetPort: intstr.FromInt(9094),
-							Protocol:   corev1.ProtocolTCP,
-						},
-						{
-							Name:       "udp-mesh",
-							Port:       9094,
-							TargetPort: intstr.FromInt(9094),
-							Protocol:   corev1.ProtocolUDP,
-						},
-					},
-				},
-			},
-			&vmv1beta1.VMServiceScrape{ObjectMeta: childObjectMeta},
-			&appsv1.StatefulSet{
-				ObjectMeta: childObjectMeta,
-				Spec: appsv1.StatefulSetSpec{
-					ServiceName:         vmalertmanagerName.Name,
-					Replicas:            ptr.To(int32(1)),
-					PodManagementPolicy: appsv1.ParallelPodManagement,
-					UpdateStrategy: appsv1.StatefulSetUpdateStrategy{
-						Type: appsv1.OnDeleteStatefulSetStrategyType,
-					},
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{
-							"app.kubernetes.io/name":      "vmalertmanager",
-							"app.kubernetes.io/instance":  name,
-							"app.kubernetes.io/component": "monitoring",
-							"managed-by":                  "vm-operator",
-						},
-					},
-					Template: corev1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{
-							Labels: map[string]string{
-								"app.kubernetes.io/name":      "vmalertmanager",
-								"app.kubernetes.io/instance":  name,
-								"app.kubernetes.io/component": "monitoring",
-								"managed-by":                  "vm-operator",
-							},
-						},
-						Spec: corev1.PodSpec{
-							Volumes: []corev1.Volume{
-								{
-									Name: "alertmanager-db",
-									VolumeSource: corev1.VolumeSource{
-										EmptyDir: &corev1.EmptyDirVolumeSource{},
-									},
-								},
-							},
-						},
-					},
-				},
-				Status: appsv1.StatefulSetStatus{
-					Replicas:           1,
-					ReadyReplicas:      1,
-					UpdatedReplicas:    1,
-					ObservedGeneration: 1,
-				},
-			},
-			&corev1.Pod{
+		preRun: func(c *k8stools.ClientWithActions, cr *vmv1beta1.VMAlertmanager) {
+			ctx := context.TODO()
+			// Create objects first
+			_ = CreateOrUpdateAlertManager(ctx, cr, c)
+
+			// Create pod for StatefulSet to simulate readiness
+			pod := &corev1.Pod{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      vmalertmanagerName.Name + "-0",
-					Namespace: namespace,
+					Namespace: vmalertmanagerName.Namespace,
 					Labels: map[string]string{
 						"app.kubernetes.io/name":      "vmalertmanager",
 						"app.kubernetes.io/instance":  name,
@@ -227,26 +133,108 @@ func Test_CreateOrUpdate_Actions(t *testing.T) {
 						{Type: corev1.PodReady, Status: corev1.ConditionTrue},
 					},
 				},
-			},
+			}
+			_ = c.Create(ctx, pod)
+
+			// Update STS status
+			sts := &appsv1.StatefulSet{}
+			if err := c.Get(ctx, vmalertmanagerName, sts); err == nil {
+				sts.Status.CurrentRevision = "v1"
+				sts.Status.UpdateRevision = "v1"
+				sts.Status.ObservedGeneration = sts.Generation
+				sts.Status.Replicas = 1
+				sts.Status.ReadyReplicas = 1
+				_ = c.Status().Update(ctx, sts)
+			}
+
+			// clear actions
+			c.Actions = nil
 		},
 	},
 		want{
 			actions: []k8stools.ClientAction{
 				{Verb: "Get", Kind: "ServiceAccount", Resource: vmalertmanagerName},
-				{Verb: "Update", Kind: "ServiceAccount", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "Role", Resource: vmalertmanagerName},
-				{Verb: "Update", Kind: "Role", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "RoleBinding", Resource: vmalertmanagerName},
-				{Verb: "Update", Kind: "RoleBinding", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "Service", Resource: vmalertmanagerName},
-				{Verb: "Delete", Kind: "Service", Resource: vmalertmanagerName},
-				{Verb: "Create", Kind: "Service", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "VMServiceScrape", Resource: vmalertmanagerName},
-				{Verb: "Update", Kind: "VMServiceScrape", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "StatefulSet", Resource: vmalertmanagerName},
-				{Verb: "Delete", Kind: "StatefulSet", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "StatefulSet", Resource: vmalertmanagerName},
-				{Verb: "Create", Kind: "StatefulSet", Resource: vmalertmanagerName},
+			},
+		})
+
+	// no update on status change
+	f(args{
+		cr: &vmv1beta1.VMAlertmanager{
+			ObjectMeta: objectMeta,
+			Spec: vmv1beta1.VMAlertmanagerSpec{
+				CommonApplicationDeploymentParams: vmv1beta1.CommonApplicationDeploymentParams{
+					ReplicaCount: ptr.To(int32(1)),
+				},
+			},
+		},
+		preRun: func(c *k8stools.ClientWithActions, cr *vmv1beta1.VMAlertmanager) {
+			ctx := context.TODO()
+			// Create objects first
+			_ = CreateOrUpdateAlertManager(ctx, cr, c)
+
+			// Create pod for StatefulSet to simulate readiness
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      vmalertmanagerName.Name + "-0",
+					Namespace: vmalertmanagerName.Namespace,
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "vmalertmanager",
+						"app.kubernetes.io/instance":  name,
+						"app.kubernetes.io/component": "monitoring",
+						"managed-by":                  "vm-operator",
+						"controller-revision-hash":    "v1",
+					},
+					OwnerReferences: []metav1.OwnerReference{
+						{
+							APIVersion: "apps/v1",
+							Kind:       "StatefulSet",
+							Name:       vmalertmanagerName.Name,
+							Controller: ptr.To(true),
+						},
+					},
+				},
+				Status: corev1.PodStatus{
+					Phase: corev1.PodRunning,
+					Conditions: []corev1.PodCondition{
+						{Type: corev1.PodReady, Status: corev1.ConditionTrue},
+					},
+				},
+			}
+			_ = c.Create(ctx, pod)
+
+			// Update STS status
+			sts := &appsv1.StatefulSet{}
+			if err := c.Get(ctx, vmalertmanagerName, sts); err == nil {
+				sts.Status.CurrentRevision = "v1"
+				sts.Status.UpdateRevision = "v1"
+				sts.Status.ObservedGeneration = sts.Generation
+				sts.Status.Replicas = 1
+				sts.Status.ReadyReplicas = 1
+				_ = c.Status().Update(ctx, sts)
+			}
+
+			// clear actions
+			c.Actions = nil
+
+			// Update status to simulate consistency
+			cr.Status.LastAppliedSpec = &cr.Spec
+		},
+	},
+		want{
+			actions: []k8stools.ClientAction{
+				{Verb: "Get", Kind: "PodDisruptionBudget", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "ServiceAccount", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "Role", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "RoleBinding", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "Service", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "VMServiceScrape", Resource: vmalertmanagerName},
+				{Verb: "Get", Kind: "StatefulSet", Resource: vmalertmanagerName},
 				{Verb: "Get", Kind: "StatefulSet", Resource: vmalertmanagerName},
 			},
 		})

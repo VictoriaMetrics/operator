38d344da8f4ac126c6f63410a5fae03881815f52
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-04-09 14:48:54 +0200

    test: add upgrade tests for 0.68.4 version

--- test/e2e/upgrade/upgrade_test.go
+++ test/e2e/upgrade/upgrade_test.go
@@ -645,6 +645,18 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 					cr.Spec.K8sCollector.Enabled = true
 					cr.Spec.ServiceAccountName = "vlagent-collector"
 				})},
+				{version: "v0.68.4", cr: with(vmagent)},
+				{version: "v0.68.4", cr: with(vmagent, func(cr *vmv1beta1.VMAgent) {
+					cr.Spec.DaemonSetMode = true
+				})},
+				{version: "v0.68.4", cr: with(vmagent, func(cr *vmv1beta1.VMAgent) {
+					cr.Spec.StatefulMode = true
+				})},
+				{version: "v0.68.4", cr: with(vlagent)},
+				{version: "v0.68.4", cr: with(vlagent, func(cr *vmv1.VLAgent) {
+					cr.Spec.K8sCollector.Enabled = true
+					cr.Spec.ServiceAccountName = "vlagent-collector"
+				})},
 			},
 		},
 		// nolint:dupl
@@ -675,6 +687,10 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				{version: "v0.68.3", cr: with(vmauth)},
 				{version: "v0.68.3", cr: with(vmalertmanager)},
 				{version: "v0.68.3", cr: with(vmanomaly)},
+				{version: "v0.68.4", cr: with(vmalert)},
+				{version: "v0.68.4", cr: with(vmauth)},
+				{version: "v0.68.4", cr: with(vmalertmanager)},
+				{version: "v0.68.4", cr: with(vmanomaly)},
 			},
 		},
 		// nolint:dupl
@@ -699,6 +715,9 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				{version: "v0.68.3", cr: with(vmsingle)},
 				{version: "v0.68.3", cr: with(vtsingle)},
 				{version: "v0.68.3", cr: with(vlsingle)},
+				{version: "v0.68.4", cr: with(vmsingle)},
+				{version: "v0.68.4", cr: with(vtsingle)},
+				{version: "v0.68.4", cr: with(vlsingle)},
 			},
 		},
 		// nolint:dupl
@@ -727,6 +746,10 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				})},
 				{version: "v0.68.3", cr: with(vlcluster)},
 				{version: "v0.68.3", cr: with(vlcluster, func(cr *vmv1.VLCluster) {
+					cr.Spec.RequestsLoadBalancer.Enabled = true
+				})},
+				{version: "v0.68.4", cr: with(vlcluster)},
+				{version: "v0.68.4", cr: with(vlcluster, func(cr *vmv1.VLCluster) {
 					cr.Spec.RequestsLoadBalancer.Enabled = true
 				})},
 			},
@@ -759,6 +782,10 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				{version: "v0.68.3", cr: with(vtcluster, func(cr *vmv1.VTCluster) {
 					cr.Spec.RequestsLoadBalancer.Enabled = true
 				})},
+				{version: "v0.68.4", cr: with(vtcluster)},
+				{version: "v0.68.4", cr: with(vtcluster, func(cr *vmv1.VTCluster) {
+					cr.Spec.RequestsLoadBalancer.Enabled = true
+				})},
 			},
 		},
 		// nolint:dupl
@@ -771,6 +798,7 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				{version: "v0.68.1", cr: with(vmcluster)},
 				{version: "v0.68.2", cr: with(vmcluster)},
 				{version: "v0.68.3", cr: with(vmcluster)},
+				{version: "v0.68.4", cr: with(vmcluster)},
 			},
 		},
 		// nolint:dupl
@@ -821,6 +849,28 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 						},
 					}
 				})},
+				{version: "v0.68.4", cr: with(vmcluster, func(cr *vmv1beta1.VMCluster) {
+					cr.Spec.RequestsLoadBalancer.Enabled = true
+					cr.Spec.VMStorage.Image.Tag = "v1.136.0-enterprise-cluster"
+					cr.Spec.VMSelect.Image.Tag = "v1.136.0-enterprise-cluster"
+					cr.Spec.VMInsert.Image.Tag = "v1.136.0-enterprise-cluster"
+					cr.Spec.RequestsLoadBalancer.Spec.Image.Tag = "v1.136.0-enterprise"
+					cr.Spec.VMStorage.VMBackup = &vmv1beta1.VMBackup{
+						Destination:                 "fs:///tmp",
+						DestinationDisableSuffixAdd: true,
+						Image: vmv1beta1.Image{
+							Tag: "v1.136.0-enterprise",
+						},
+					}
+					cr.Spec.License = &vmv1beta1.License{
+						KeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "license",
+							},
+							Key: "key",
+						},
+					}
+				})},
 			},
 			envs: map[string]string{
 				"VM_LOOPBACK": "localhost",
@@ -834,6 +884,7 @@ var _ = Describe("operator upgrade", Label("upgrade"), func() {
 				{version: "v0.68.1", cr: with(vmdistributed)},
 				{version: "v0.68.2", cr: with(vmdistributed)},
 				{version: "v0.68.3", cr: with(vmdistributed)},
+				{version: "v0.68.4", cr: with(vmdistributed)},
 			},
 		},
 	}))

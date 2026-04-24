f57ede8eadc264054a57d8f8ab05bff3258d86e9
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-04-24 12:42:19 +0200

    refactor: extract PodTemplateParams

---
+++ internal/controller/operator/factory/build/podtemplate.go
@@ -0,0 +1,33 @@
+package build
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
+
+	vmv1beta1 "github.com/VictoriaMetrics/operator/api/operator/v1beta1"
+)
+
+// PodTemplateAddCommonParams populates common pod-level fields on a PodTemplateSpec
+func PodTemplateAddCommonParams(dst *corev1.PodTemplateSpec, params *vmv1beta1.CommonAppsParams) {
+	dst.Spec.Affinity = params.Affinity
+	dst.Spec.Tolerations = params.Tolerations
+	dst.Spec.SchedulerName = params.SchedulerName
+	dst.Spec.RuntimeClassName = params.RuntimeClassName
+	dst.Spec.HostAliases = params.HostAliases
+	if len(params.HostAliasesUnderScore) > 0 {
+		dst.Spec.HostAliases = params.HostAliasesUnderScore
+	}
+	dst.Spec.PriorityClassName = params.PriorityClassName
+	dst.Spec.HostNetwork = params.HostNetwork
+	dst.Spec.DNSPolicy = params.DNSPolicy
+	dst.Spec.DNSConfig = params.DNSConfig
+	dst.Spec.NodeSelector = params.NodeSelector
+	dst.Spec.SecurityContext = addStrictSecuritySettingsToPod(params)
+	dst.Spec.TerminationGracePeriodSeconds = params.TerminationGracePeriodSeconds
+	dst.Spec.TopologySpreadConstraints = params.TopologySpreadConstraints
+	dst.Spec.ImagePullSecrets = params.ImagePullSecrets
+	dst.Spec.ReadinessGates = params.ReadinessGates
+	if params.DisableAutomountServiceAccountToken {
+		dst.Spec.AutomountServiceAccountToken = ptr.To(false)
+	}
+}

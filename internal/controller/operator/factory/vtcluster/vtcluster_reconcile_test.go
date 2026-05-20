abf8d68f2dc279c196a0e553c46fcf49ec4b76a7
Vadim Rutkovsky <vadim@vrutkovs.eu>
2026-05-20 08:45:28 +0200

    test: add unit tests for HPA-controlled requestLoadBalancer replica count

--- internal/controller/operator/factory/vtcluster/vtcluster_reconcile_test.go
+++ internal/controller/operator/factory/vtcluster/vtcluster_reconcile_test.go
@@ -293,3 +293,59 @@ func TestCreateOrUpdate_Paused(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int32(1), *sts.Spec.Replicas)
 }
+
+func TestCreateOrUpdate_LBDeploymentWithHPA(t *testing.T) {
+	cr := &vmv1.VTCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "example-cluster",
+			Namespace: "default",
+		},
+		Spec: vmv1.VTClusterSpec{
+			RequestsLoadBalancer: vmv1beta1.VMAuthLoadBalancer{
+				Enabled: true,
+				Spec: vmv1beta1.VMAuthLoadBalancerSpec{
+					CommonAppsParams: vmv1beta1.CommonAppsParams{
+						ReplicaCount: ptr.To(int32(1)),
+					},
+					HPA: &vmv1beta1.EmbeddedHPA{
+						MinReplicas: ptr.To(int32(1)),
+						MaxReplicas: 5,
+					},
+				},
+			},
+			Storage: &vmv1.VTStorage{
+				CommonAppsParams: vmv1beta1.CommonAppsParams{ReplicaCount: ptr.To(int32(0))},
+			},
+			Select: &vmv1.VTSelect{
+				CommonAppsParams: vmv1beta1.CommonAppsParams{ReplicaCount: ptr.To(int32(0))},
+			},
+			Insert: &vmv1.VTInsert{
+				CommonAppsParams: vmv1beta1.CommonAppsParams{ReplicaCount: ptr.To(int32(0))},
+			},
+		},
+	}
+	depNSN := types.NamespacedName{Namespace: cr.Namespace, Name: cr.PrefixedName(vmv1beta1.ClusterComponentBalancer)}
+	fclient := k8stools.GetTestClientWithObjects([]runtime.Object{cr})
+	ctx := context.TODO()
+	build.AddDefaults(fclient.Scheme())
+	fclient.Scheme().Default(cr)
+
+	// initial - 1 replica
+	assert.NoError(t, CreateOrUpdate(ctx, fclient, cr))
+
+	var dep appsv1.Deployment
+	assert.NoError(t, fclient.Get(ctx, depNSN, &dep))
+	assert.Equal(t, int32(1), *dep.Spec.Replicas)
+
+	// simulate HPA scaling the Deployment to 3 replicas
+	dep.Spec.Replicas = ptr.To(int32(3))
+	assert.NoError(t, fclient.Update(ctx, &dep))
+
+	// set desired replica count to 2
+	cr.Spec.RequestsLoadBalancer.Spec.ReplicaCount = ptr.To(int32(2))
+	assert.NoError(t, CreateOrUpdate(ctx, fclient, cr))
+
+	// HPA-managed replica count must not be overwritten
+	assert.NoError(t, fclient.Get(ctx, depNSN, &dep))
+	assert.Equal(t, int32(3), *dep.Spec.Replicas)
+}
